ca4346982b4ba288d0dbfbe3b398318b24290d88
eric <eric>
2002-04-08 15:48:11 +0000

    affichage log si au moins 1

--- Share/logmsg.js
+++ Share/logmsg.js
@@ -3,16 +3,25 @@ function displayLogMsg(logmsg) {
     var log=top.foot.document.getElementById('slog');
     if (log) {
       var classn = 'CORETblCell'
-      if (log.options.length > 0)
+  
+      
+      if ((log.options) && (log.options.length > 0)) {
 	if (log.options[log.options.length-1].className == "CORETblCell") 
 	  classn='CORETblCellAltern';
-      for (var i=0;i<logmsg.length;i++) {
         var k=log.options.length;
+      } else {
+        var k=0;	
+      }
+      for (var i=0;i<logmsg.length;i++) {
         log.options[k]=new Option(logmsg[i]);
         log.options[k].className=classn;
-	
+	k=log.options.length;
       }
       log.selectedIndex=log.options.length-1;
+   
+      if ((! log.options) || (log.options.length == 0)) log.style.display='none';
+      else log.style.display='inline';
     }
+    
   }
 }
