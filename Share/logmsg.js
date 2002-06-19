966654fad9e1dd2e1fdd7915daf53f267d690e2b
eric <eric>
2002-06-19 12:25:23 +0000

    supp js warning

--- Share/logmsg.js
+++ Share/logmsg.js
@@ -2,16 +2,14 @@ function displayLogMsg(logmsg) {
   if (top.foot) {
     var log=top.foot.document.getElementById('slog');
     if (log) {
-      var classn = 'CORETblCell'
-  
-      
+      var classn = 'CORETblCell'        
+      var k=0;
+
       if ((log.options) && (log.options.length > 0)) {
 	if (log.options[log.options.length-1].className == "CORETblCell") 
 	  classn='CORETblCellAltern';
-        var k=log.options.length;
-      } else {
-        var k=0;	
-      }
+        k=log.options.length;
+      } 
       for (var i=0;i<logmsg.length;i++) {
         log.options[k]=new Option(logmsg[i]);
         log.options[k].className=classn;
