d97b0663f1422d9ff3a526fb4fdc8e22ad4e0507
eric <eric>
2002-07-11 13:16:59 +0000

    ajout mode menu

--- Share/subwindow.js
+++ Share/subwindow.js
@@ -18,3 +18,24 @@ function subwindow(h, w, name, url) {
   return false;
 }
 
+// with menu
+function subwindowm(h, w, name, url) {
+
+   var screen_width, screen_height;
+   var win_top, win_left;
+   var HelpWin;
+   screen_height        = 0;     screen_width      = 0;
+   win_top              = 0;     win_left          = 0;
+   if (window.innerWidth) screen_width = window.innerWidth;
+   if (window.innerHeight) screen_height = window.innerHeight;
+   win_top  = screen_height - h - 20;
+   win_left = screen_width  - w  - 20;
+   me  = window.open(
+              url,
+              name,
+              'menubar=yes,resizable=yes,scrollbars=yes,width='+w+',height='+h+',top='+win_top+',left='+win_left);
+  me.focus();
+
+    
+  return false;
+}
