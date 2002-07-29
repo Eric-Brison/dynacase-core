447fddb9d8df49c216d838bb655a7f21ab0e74c8
eric <eric>
2002-07-29 09:50:48 +0000

    modif pour IR logmsg externe aux frames

--- Share/logmsg.js
+++ Share/logmsg.js
@@ -1,30 +1,38 @@
 function displayLogMsg(logmsg) {
+
+  if (logmsg.length == 0) return;
+  
   var log=false;
   if (top.foot) {
     log=top.foot.document.getElementById('slog');
   } else {
-    wfoot = window.open('','foot','');
-    log=wfoot.document.getElementById('slog');
-  }
-    if (log) {
-      var classn = 'CORETblCell'        
-      var k=0;
-
-      if ((log.options) && (log.options.length > 0)) {
-	if (log.options[log.options.length-1].className == "CORETblCell") 
-	  classn='CORETblCellAltern';
-        k=log.options.length;
-      } 
-      for (var i=0;i<logmsg.length;i++) {
-        log.options[k]=new Option(logmsg[i]);
-        log.options[k].className=classn;
-	k=log.options.length;
+    // redirect to foot function
+      if (window .name != "foot") {
+	var wfoot = window.open('','foot','');
+	wfoot.displayLogMsg(logmsg);
       }
-      log.selectedIndex=log.options.length-1;
-   
-      if ((! log.options) || (log.options.length == 0)) log.style.display='none';
-      else log.style.display='inline';
-    }
+    return;
+  }
+  
+  if (log) {
+    var classn = 'CORETblCell'        
+      var k=0;
     
+    if ((log.options) && (log.options.length > 0)) {
+      if (log.options[log.options.length-1].className == "CORETblCell") 
+	classn='CORETblCellAltern';
+      k=log.options.length;
+    } 
+    for (var i=0;i<logmsg.length;i++) {
+      log.options[k]=new Option(logmsg[i],'',false,false);
+      log.options[k].className=classn;
+      k=log.options.length;
+    }
+    log.selectedIndex=log.options.length-1;
+    
+    if ((! log.options) || (log.options.length == 0)) log.style.display='none';
+    else log.style.display='inline';
+  }
+  
   
 }
