e2f4f265eb9b3f6ed13696f74bf9985c31778647
eric <eric>
2002-08-19 12:34:59 +0000

    modif apache.conf pour acces fichier html et css (doc)

--- Action/Authent/authent.php
+++ Action/Authent/authent.php
@@ -2,21 +2,23 @@
 include_once('Class.User.php');
 include_once('Class.Session.php');
 
-  function authenticate() {
-   Header( "WWW-Authenticate: Basic realm=\"WHAT Connection\", stale=FALSE");
-   Header( "HTTP/1.0 401 Unauthorized");
-   // Header("Location:guest.php");
-   echo "Vous devez entrer un nom d'utilisateur valide et un mot de passe correct pour accéder à cette ressource\n";
-   exit;
-  }
+function authenticate() {
+  //   Header( "WWW-Authenticate: Basic realm=\"WHAT Connection\", stale=FALSE");
+  Header( "WWW-Authenticate: Basic realm=\"WHAT Connection\"");
+  Header( "HTTP/1.0 401 Unauthorized");
+  // Header("Location:guest.php");
+  echo _("Vous devez entrer un nom d'utilisateur valide et un mot de passe correct pour accéder à cette ressource");
+  exit;
+}
 
 //print "$PHP_AUTH_USER $SeenBefore $OldAuth";
-  if(!isset($PHP_AUTH_USER) || ($SeenBefore == 1 && !strcmp($OldAuth, $PHP_AUTH_USER)) ) {
-   authenticate();
-  }
-  else {
+//if(!isset($PHP_AUTH_USER) || ($SeenBefore == 1 && !strcmp($OldAuth, $PHP_AUTH_USER)) ) {
+if(!isset($PHP_AUTH_USER) || ($SeenBefore == 1 ))  {
+  authenticate();
+}
+else {
 
-    Header("Location:../index.php?sole=R");
-    exit;
-  }
+  Header("Location:../index.php?sole=R");
+  exit;
+}
 ?>
