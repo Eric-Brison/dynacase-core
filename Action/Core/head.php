771af817b7f61b16d45ed2de8f404250bdc239b1
eric <eric>
2002-12-18 15:03:17 +0000

    ajout style Aqua

--- Action/Core/head.php
+++ Action/Core/head.php
@@ -1,6 +1,6 @@
 <?php
 // ---------------------------------------------------------------
-// $Id: head.php,v 1.11 2002/08/26 13:04:58 eric Exp $
+// $Id: head.php,v 1.12 2002/12/18 15:03:17 eric Exp $
 // $Source: /home/cvsroot/anakeen/freedom/core/Action/Core/head.php,v $
 // ---------------------------------------------------------------
 //  O   Anakeen - 2001
@@ -75,6 +75,8 @@ function head(&$action) {
       $appli["descriptionsla"]= addslashes($appli["description"]); // because its in between '' in layout
       if ($appli["machine"] != "") $appli["pubdir"]= "http://".$appli["machine"]."/what";
       else $appli["pubdir"]=$action->getParam("CORE_PUBURL");
+
+      $appli["iconsrc"]=$action->GetImageUrl($appli["icon"]);
       $tab[$i++]=$appli;
     }
   }
