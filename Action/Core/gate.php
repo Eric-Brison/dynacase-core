c723e4e44abdcd886659c8f797ee59109bf798be
eric <eric>
2003-04-14 12:47:12 +0000

    correction portail relatif

--- Action/Core/gate.php
+++ Action/Core/gate.php
@@ -1,6 +1,6 @@
 <?php
 // ---------------------------------------------------------------
-// $Id: gate.php,v 1.1 2003/04/07 12:33:04 eric Exp $
+// $Id: gate.php,v 1.2 2003/04/14 12:47:12 eric Exp $
 // $Source: /home/cvsroot/anakeen/freedom/core/Action/Core/gate.php,v $
 // ---------------------------------------------------------------
 //  O   Anakeen - 2001
@@ -34,28 +34,56 @@ function gate(&$action) {
   // geometry set
   $tgeo=explode(",",$geo);
   if (count($tgeo) != 6) {
-    $action->lay->set("G1W",($turl[0]=="")?"0pt":"*");
-    $action->lay->set("G2W",($turl[2]=="")?"0pt":"*");
-    $action->lay->set("G3W",($turl[4]=="")?"0pt":"*");
-    $action->lay->set("G1H",($turl[1]=="")?"0pt":"*");
-    $action->lay->set("G2H",($turl[3]=="")?"0pt":"*");
-    $action->lay->set("G3H",($turl[5]=="")?"0pt":"*");
+    $G1W=($turl[0]=="")?"0%":"*";
+    $G2W=($turl[2]=="")?"0%":"*";
+    $G3W=($turl[4]=="")?"0%":"*";
+    $G1H=($turl[1]=="")?"0%":"*";
+    $G2H=($turl[3]=="")?"0%":"*";
+    $G3H=($turl[5]=="")?"0%":"*";
   } else {
 
     list($G1W,$G1H)=explode("x",$tgeo[0]);
     list($G2W,$G2H)=explode("x",$tgeo[2]);
     list($G3W,$G3H)=explode("x",$tgeo[4]);
+  }
+    if (($turl[0]=="") && ($turl[1]=="")) $G1W="0%";
+    if (($turl[2]=="") && ($turl[3]=="")) $G2W="0%";
+    if (($turl[4]=="") && ($turl[5]=="")) $G3W="0%";
+    if ($turl[0]=="") $G1H="0%";
+    if ($turl[2]=="") $G2H="0%";
+    if ($turl[4]=="") $G3H="0%";
 
-    if (($turl[0]=="") && ($turl[1]=="")) $G1W="0pt";
-    if (($turl[2]=="") && ($turl[3]=="")) $G2W="0pt";
-    if (($turl[4]=="") && ($turl[5]=="")) $G3W="0pt";
+    if ($turl[0]!="") {
+      if ($G1H == "0%") $G1H="10%";
+      if ($G1W == "0%") $G1W="10%";
+    }
+    if ($turl[1]!="") {
+      //if ($G1H == "0%") $G1H="10%";
+      if ($G2W == "0%") $G2W="10%";
+    }
+    if ($turl[2]!="") {
+      if ($G2H == "0%") $G2H="10%";
+      if ($H2W == "0%") $G2W="10%";
+    }
+    if ($turl[3]!="") {
+      //if ($G2H == "0%") $G2H="10%";
+      if ($G2W == "0%") $G2W="10%";
+    }
+    if ($turl[4]!="") {
+      if ($G3H == "0%") $G3H="10%";
+      if ($G3W == "0%") $G3W="10%";
+    }
+    if ($turl[5]!="") {
+      //if ($G2H == "0%") $G2H="10%";
+      if ($G3W == "0%") $G3W="10%";
+    }
     $action->lay->set("G1W",$G1W);
     $action->lay->set("G2W",$G2W);
     $action->lay->set("G3W",$G3W);
     $action->lay->set("G1H",$G1H);
     $action->lay->set("G2H",$G2H);
     $action->lay->set("G3H",$G3H);
-  }
+  
 
     // url set
   while (list($k,$url) = each($turl)) {
