425c8525b80b68d2444825aa62c994cba0fb1e07
eric <eric>
2003-06-02 09:39:10 +0000

    ajout LC_MONETARY

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // ---------------------------------------------------------------
-// $Id: index.php,v 1.14 2002/08/26 13:04:58 eric Exp $
+// $Id: index.php,v 1.15 2003/06/02 09:39:10 eric Exp $
 // $Source: /home/cvsroot/anakeen/freedom/core/index.php,v $
 // ---------------------------------------------------------------
 //  O   Anakeen - 2001
@@ -182,6 +182,7 @@ if ($action->Read("navigator","") == "") {
 }
 // init for gettext
 setlocale(LC_MESSAGES,$action->Getparam("CORE_LANG"));  
+setlocale(LC_MONETARY, $action->Getparam("CORE_LANG"));
 //print $action->Getparam("CORE_LANG");
 putenv ("LANG=".$action->Getparam("CORE_LANG")); // needed for old Linux kernel < 2.4
 bindtextdomain ("what", "/home/httpd/what/locale");
