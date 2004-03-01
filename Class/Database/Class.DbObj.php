26e7af977579378dcf854e1be4ae01322d6d50ae
eric <eric>
2004-03-01 08:39:21 +0000

    *** empty log message ***

--- Class/Database/Class.DbObj.php
+++ Class/Database/Class.DbObj.php
@@ -4,7 +4,7 @@
  * based on the description of a DB Table. 
  *
  * @author Anakeen 2000 
- * @version $Id: Class.DbObj.php,v 1.26 2004/02/24 08:22:02 eric Exp $
+ * @version $Id: Class.DbObj.php,v 1.27 2004/03/01 08:39:21 eric Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package WHAT
  * @subpackage CORE
@@ -14,7 +14,7 @@
 
 // ---------------------------------------------------------------------------
 // Db Object
-// @version $Id: Class.DbObj.php,v 1.26 2004/02/24 08:22:02 eric Exp $
+// @version $Id: Class.DbObj.php,v 1.27 2004/03/01 08:39:21 eric Exp $
 // ---------------------------------------------------------------------------
 // Anakeen 2000 - yannick.lebriquer@anakeen.com
 // ---------------------------------------------------------------------------
@@ -40,7 +40,7 @@ include_once('Class.Log.php');
 include_once('Class.Cache.php');
 include_once('Lib.Common.php');
 
-$CLASS_DBOBJ_PHP = '$Id: Class.DbObj.php,v 1.26 2004/02/24 08:22:02 eric Exp $';
+$CLASS_DBOBJ_PHP = '$Id: Class.DbObj.php,v 1.27 2004/03/01 08:39:21 eric Exp $';
 
 /**
  * This class is a generic DB Class that can be used to create objects
@@ -516,7 +516,7 @@ function Create($nopost=false)
       $this->log->debug("Init : {$this->sqlinit}");
     }
     if ($msg != '') {
-      $this->log->info("DbObj::Create $msg");
+      $this->log->info("DbObj::Create $msg");   
       return $msg;
     }
     if (!$nopost) $this->PostInit();
