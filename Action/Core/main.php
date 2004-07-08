7d8e1c6c2e26bc50c988e02eb3d2cc87dda9a1d7
eric <eric>
2004-07-08 08:52:03 +0000

    use of FREEGATE

--- Action/Core/main.php
+++ Action/Core/main.php
@@ -3,7 +3,7 @@
  * Main page for WHAT
  *
  * @author Anakeen 2000 
- * @version $Id: main.php,v 1.6 2004/07/05 13:44:24 eric Exp $
+ * @version $Id: main.php,v 1.7 2004/07/08 08:52:03 eric Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package WHAT
  * @subpackage CORE
@@ -23,14 +23,14 @@ function main(&$action) {
   $action->lay->set("SESSION",$action->session->id);
 
       
-  if ($action->parent->exists("FREEGATE")) {    
-  $action->lay->set("appd", "FREEGATE");
-  $action->lay->set("actd", "FREEGATE_VIEWGATE");
+  if ($action->parent->exists("FREEGATE") && ($action->getParam("GATE_USEOLD")!="yes")) {
+    $action->lay->set("appd", "FREEGATE");
+    $action->lay->set("actd", "FREEGATE_VIEWGATE");
   } else {
-  $appd = GetHttpVars("appd","CORE");
-  $actd = GetHttpVars("actd","GATE");
-  $action->lay->set("appd", $appd);
-  $action->lay->set("actd", $actd);
+    $appd = GetHttpVars("appd","CORE");
+    $actd = GetHttpVars("actd","GATE");
+    $action->lay->set("appd", $appd);
+    $action->lay->set("actd", $actd);
   }
 
   // reopen a new session
