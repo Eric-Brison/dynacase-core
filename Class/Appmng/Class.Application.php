b5b7fd386fc266a85a37fe9deb3f190ec49793d4
eric <eric>
2005-01-21 17:48:26 +0000

    Comment

--- Class/Appmng/Class.Application.php
+++ Class/Appmng/Class.Application.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Generated Header (not documented yet)
+ * Application Class
  *
  * @author Anakeen 2000 
- * @version $Id: Class.Application.php,v 1.34 2005/01/07 16:59:39 eric Exp $
+ * @version $Id: Class.Application.php,v 1.35 2005/01/21 17:48:26 eric Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package WHAT
  * @subpackage CORE
@@ -11,29 +11,8 @@
  /**
  */
 
-// ---------------------------------------------------------------------------
-// PHP PROMAN Task Class
-// ---------------------------------------------------------------------------
-// anakeen 2000 - Marianne Le Briquer
-// ---------------------------------------------------------------------------
-// This program is free software; you can redistribute it and/or modify 
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or (at
-//  your option) any later version.
-//
-// This program is distributed in the hope that it will be useful, but 
-// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
-// for more details.
-// 
-// You should have received a copy of the GNU General Public License along 
-// with this program; if not, write to the Free Software Foundation, Inc.,
-// 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-// ---------------------------------------------------------------------------
-//  $Id: Class.Application.php,v 1.34 2005/01/07 16:59:39 eric Exp $
-//
 
-$CLASS_APPLICATION_PHP = '$Id: Class.Application.php,v 1.34 2005/01/07 16:59:39 eric Exp $';
+
 include_once('Class.DbObj.php');
 include_once('Class.QueryDb.php');
 include_once('Class.Action.php');
