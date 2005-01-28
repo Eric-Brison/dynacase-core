2d121aaacc7d8245c81667408ac60926d7c43600
eric <eric>
2005-01-28 13:23:14 +0000

    Open in new window if Ctrl-Click

--- Action/Core/lauch_action.js
+++ Action/Core/lauch_action.js
@@ -1,6 +1,18 @@
-function lauch_action(img, appname, descr) {
+function lauch_action(event, img, appname, descr) {
+  if (!event) event=window.event;
+  ctrlKey = event.ctrlKey;
+  
+  if (ctrlKey) {
+    if (event.stopPropagation) event.stopPropagation();
+    else event.cancelBubble=true;
+    if (event.preventDefault) event.preventDefault();
+    else event.returnValue=true;
 
-  if (parent && parent.body) parent.body.location.replace('[CORE_BASEURL]app='+appname);
+  
+    
+  }
+
+  if (parent && parent.body && (!ctrlKey)) parent.body.location.replace('[CORE_BASEURL]app='+appname);
   else subwindow([FDL_VD2SIZE],[FDL_HD2SIZE],'wbody'+appname,'[CORE_BASEURL]app='+appname);
 
   top.document.title="WHAT: "+descr;
