bb68ea081dce18d5d5339c9da3dafbe2704b6810
eric <eric>
2005-05-13 16:07:03 +0000

    View static parameters

--- Zone/Appmng/param_list.php
+++ Zone/Appmng/param_list.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Generated Header (not documented yet)
+ * Display parameters
  *
  * @author Anakeen 2000 
- * @version $Id: param_list.php,v 1.7 2005/04/14 09:53:50 marc Exp $
+ * @version $Id: param_list.php,v 1.8 2005/05/13 16:07:03 eric Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package WHAT
  * @subpackage APPMNG
@@ -11,66 +11,6 @@
  /**
  */
 
-// ---------------------------------------------------------------
-// $Id: param_list.php,v 1.7 2005/04/14 09:53:50 marc Exp $
-// $Source: /home/cvsroot/anakeen/freedom/core/Zone/Appmng/param_list.php,v $
-// ---------------------------------------------------------------
-//  O   Anakeen - 2000
-// O*O  Anakeen development team
-//  O   dev@anakeen.com
-// ---------------------------------------------------------------
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or (at
-//  your option) any later version.
-//
-// This program is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
-// for more details.
-//
-// You should have received a copy of the GNU General Public License along
-// with this program; if not, write to the Free Software Foundation, Inc.,
-// 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-// ---------------------------------------------------------------
-// $Log: param_list.php,v $
-// Revision 1.7  2005/04/14 09:53:50  marc
-// Show/Hide parameters by application
-//
-// Revision 1.6  2003/08/18 15:46:42  eric
-// phpdoc
-//
-// Revision 1.5  2002/08/26 13:04:58  eric
-// application multi-machine
-//
-// Revision 1.4  2002/05/28 09:00:26  eric
-// mise au point paramètres & style
-//
-// Revision 1.3  2002/05/27 14:51:30  eric
-// ajout gestion des styles
-//
-// Revision 1.2  2002/05/24 09:23:07  eric
-// changement structure table paramv
-//
-// Revision 1.1  2002/05/23 16:14:40  eric
-// paramètres utilisateur
-//
-// Revision 1.2  2002/03/21 17:52:38  eric
-// prise en compte application répartie sur plusieurs machines
-//
-// Revision 1.1  2002/01/08 12:41:33  eric
-// first
-//
-// Revision 1.3  2001/09/10 16:46:49  eric
-// modif pour libwhat 4.8 : accessibilté objet
-//
-// Revision 1.2  2001/02/26 16:57:14  yannick
-// remove tablelayout bug
-//
-// Revision 1.1  2001/01/29 15:50:59  marianne
-// prise en compte de la gestion des parametres
-//
-// ---------------------------------------------------------------
 include_once("Class.TableLayout.php");
 include_once("Class.QueryDb.php");
 include_once("Class.Param.php");
@@ -162,6 +102,8 @@ function param_list(&$action) {
 	  else $tincparam[$k]["classtype"]="gparam";
 	$tincparam[$k]["sval"]=addslashes($v["val"]);
 	
+	$tincparam[$k]["colorstatic"]=($v["kind"]=="static")?"#666666":"";
+
 	// force type user if user mode
 	  if ($userid > 0) $tincparam[$k]["type"]=PARAM_USER.$userid;
 	else if ($styleid != "") $tincparam[$k]["type"]=PARAM_STYLE.$styleid;
