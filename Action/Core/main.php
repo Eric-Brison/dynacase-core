71bd7d4b4f7bd37d1ed750e03ce278501fbd1f3a
marc <marc>
2005-10-18 09:41:26 +0000

    CORE_FRONTPAGE describes header/footer/main : application, action and width

--- Action/Core/main.php
+++ Action/Core/main.php
@@ -3,7 +3,7 @@
  * Main page for WHAT
  *
  * @author Anakeen 2000 
- * @version $Id: main.php,v 1.9 2005/06/10 13:05:18 eric Exp $
+ * @version $Id: main.php,v 1.10 2005/10/18 09:41:26 marc Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package WHAT
  * @subpackage CORE
@@ -22,17 +22,73 @@ function main(&$action) {
   $action->lay->set("APP_TITLE", $app->description);
   $action->lay->set("SESSION",$action->session->id);
 
-      
+  $mainpdescr = $action->getParam("CORE_FRONTPAGE", "");
+ 
+  $izone = 0; 
+  $zonedef = array();
+  $isOk = array( false, false, false );
+  if ($mainpdescr!="") {
+    $tzone = explode("|", $mainpdescr);
+    if ($count($tzone)!=3) continue;
+    foreach ($tzone as $k => $v) {
+      $zargs = explode(":", $v);
+      if (count($zargs)!=3) continue;
+      if ($zargs[0]=="" || $zargs[1]=="" || $zargs[3]=="") continue;
+      $zonedef[$izone]["app"] = $zargs[0];
+      $zonedef[$izone]["action"] = $zargs[1];
+      $zonedef[$izone]["size"] = $zargs[2];
+      $isOk[$izone] = true;
+    }
+    $izone++; 
+  }
+  
+    
+  $headerSize = "52";
+  $headerApp  = "CORE";
+  $headerAct  = "HEAD";
+  $mainSize = "*";
+  $mainApp  = "CORE";
+  $mainAct  = "GATE";
+  $footerSize = "35";
+  $footerApp  = "CORE";
+  $footerAct  = "FOOTER";
+
+
   if ($action->parent->exists("FREEGATE") && ($action->getParam("GATE_USEOLD")!="yes")) {
-    $action->lay->set("appd", "FREEGATE");
-    $action->lay->set("actd", "FREEGATE_VIEWGATE");
+
+    $mainApp = "FREEGATE";
+    $mainAct = "FREEGATE_VIEWGATE";
+
   } else {
-    $appd = GetHttpVars("appd","CORE");
-    $actd = GetHttpVars("actd","GATE");
-    $action->lay->set("appd", $appd);
-    $action->lay->set("actd", $actd);
+  
+    if ($isOk[0] && $isOk[0] && $isOk[0]) {
+      
+      $headerSize = $zonedef[0]["size"];
+      $headerApp  = $zonedef[0]["app"];
+      $headerAct  = $zonedef[0]["action"];
+      
+      $mainSize = $zonedef[1]["size"];
+      $mainApp  = $zonedef[1]["app"];
+      $mainAct  = $zonedef[1]["action"];
+      
+      $footerSize = $zonedef[1]["size"];
+      $footerApp  = $zonedef[1]["app"];
+      $footerAct  = $zonedef[1]["action"];
+      
+    } 
   }
 
+  $action->lay->set("headerSize", $headerSize);
+  $action->lay->set("headerApp", $headerApp);
+  $action->lay->set("headerAct", $headerAct);
+  $action->lay->set("mainSize", $mainSize);
+  $action->lay->set("mainApp", $mainApp);
+  $action->lay->set("mainAct", $mainAct);
+  $action->lay->set("footerSize", $footerSize);
+  $action->lay->set("footerApp", $footerApp);
+  $action->lay->set("footerAct", $footerAct);
+
+  
   // reopen a new session
   $action->parent->session->Set("");
   $action->parent->SetSession($action->parent->session);
