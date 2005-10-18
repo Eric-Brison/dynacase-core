0769312dc8c4d990cd3c17d8c0e206299ff6304d
eric <eric>
2005-10-18 15:36:24 +0000

    *** empty log message ***

--- Share/wgcheck.php
+++ Share/wgcheck.php
@@ -3,7 +3,7 @@
  * Util function for update and initialize application
  *
  * @author Anakeen 2005
- * @version $Id: wgcheck.php,v 1.1 2005/10/18 14:12:42 eric Exp $
+ * @version $Id: wgcheck.php,v 1.2 2005/10/18 15:36:24 eric Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package WHAT
  * @subpackage CORE
@@ -25,6 +25,10 @@ TR.U {background-color:yellow;}
 </style>
 </head>
 <body style="background-image:url('CORE/Images/bg.gif')">
+
+
+<div style="width:80%;border: groove 4px red">
+<a href="#"><img align="right" width="100px" src="CORE/Images/freeeye.png"></a>
 <H1>Applications state</H1>
 <table cellspacing="0"><tr><th>Application</th><th>DB version</th><th>file version</th><th>State</th><th>Machine</th></tr>
 <?php
@@ -45,5 +49,5 @@ foreach ($applications as $k=>$v) {
 		$v["machine"]);
 }
 ?>
-</table>
+</table></div>
 </body></html>
