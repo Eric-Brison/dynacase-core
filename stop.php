86974c3e2f88dd545f0411e95bfbf3654b08af97
eric <eric>
2007-03-06 18:55:58 +0000

    New logo

--- stop.php
+++ stop.php
@@ -4,7 +4,7 @@
  * replace index.php when wstop is invocated (RPM update)
  *
  * @author Anakeen 2002
- * @version $Id: stop.php,v 1.3 2005/10/18 14:12:42 eric Exp $
+ * @version $Id: stop.php,v 1.4 2007/03/06 18:57:03 eric Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package WHAT
  * @subpackage CORE
@@ -18,9 +18,7 @@
 <BODY  style="background-image:url('CORE/Images/bg.gif')">
 <div align="center">
 <div style="width:80%;border: groove 4px red">
-<IMG align="right" width="200px" src="CORE/Images/freeeye.png">
-<IMG align="right" width="100px" src="CORE/Images/freeeye.png">
-<IMG align="right" width="50px" src="CORE/Images/freeeye.png">
+<IMG align="right"  src="CORE/Images/freeeye.png">
 <H1 align="left">FREEDOM Databases must be updated.</H1>
 
 <H2 align="left">Update is in progress.</H2>
