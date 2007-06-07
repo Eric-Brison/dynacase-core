c79d011c9cc804f106997f80936e7a8afc7e2309
eric <eric>
2007-06-07 16:19:44 +0000

    Correct for NOTICE warning

--- Class/Database/Class.DbObj.php
+++ Class/Database/Class.DbObj.php
@@ -4,7 +4,7 @@
  * based on the description of a DB Table. 
  *
  * @author Anakeen 2000 
- * @version $Id: Class.DbObj.php,v 1.51 2007/06/07 15:25:04 eric Exp $
+ * @version $Id: Class.DbObj.php,v 1.52 2007/06/07 16:19:44 eric Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package WHAT
  * @subpackage CORE
@@ -14,7 +14,7 @@
 
 // ---------------------------------------------------------------------------
 // Db Object
-// @version $Id: Class.DbObj.php,v 1.51 2007/06/07 15:25:04 eric Exp $
+// @version $Id: Class.DbObj.php,v 1.52 2007/06/07 16:19:44 eric Exp $
 // ---------------------------------------------------------------------------
 // Anakeen 2000 - yannick.lebriquer@anakeen.com
 // ---------------------------------------------------------------------------
@@ -39,7 +39,7 @@
 include_once('Class.Log.php');
 include_once('Lib.Common.php');
 
-$CLASS_DBOBJ_PHP = '$Id: Class.DbObj.php,v 1.51 2007/06/07 15:25:04 eric Exp $';
+$CLASS_DBOBJ_PHP = '$Id: Class.DbObj.php,v 1.52 2007/06/07 16:19:44 eric Exp $';
 
 /**
  * This class is a generic DB Class that can be used to create objects
@@ -395,6 +395,7 @@ function Add($nopost=false)
  */
 function Modify($nopost=false,$sfields="",$nopre=false)
   {
+    $msg='';
     if ($this->dbid == -1) return FALSE;
     if (!$nopre) $msg=$this->PreUpdate();
     if ($msg!='') return $msg;
