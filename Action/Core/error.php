240150ee46ad5b9baffca28d0884d4bd63602801
eric <eric>
2008-02-12 15:19:28 +0000

    Don't delete FT_ERROR in case of double call (from MS-WORD for example)

--- Action/Core/error.php
+++ Action/Core/error.php
@@ -3,7 +3,7 @@
  * Generated Header (not documented yet)
  *
  * @author Anakeen 2000 
- * @version $Id: error.php,v 1.11 2005/06/10 13:05:18 eric Exp $
+ * @version $Id: error.php,v 1.12 2008/02/12 15:19:28 eric Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package WHAT
  * @subpackage CORE
@@ -11,7 +11,7 @@
  /**
  */
 
-// $Id: error.php,v 1.11 2005/06/10 13:05:18 eric Exp $
+// $Id: error.php,v 1.12 2008/02/12 15:19:28 eric Exp $
 
 //
 //
@@ -27,16 +27,14 @@ function error(&$action) {
 
   $action->lay->set("serror", str_replace("\n","\\n",addslashes($action->Read("FT_ERROR","Session Error"))));
 
+
+  if (strlen($action->Read("FT_ERROR"))==0) print "<h1>Empty</h1>";
   $action->lay->set("appname", _($app->description));
 
   if ($app->name != "CORE") {
-    $app = new Application();
-  
+    $app = new Application();  
     $app->Set($action->Read("FT_ERROR_APP","CORE"), $action->parent);
-
     $actname = $action->Read("FT_ERROR_ACT","");
-
-
 
     $action->lay->set("appact", "");
     if ($actname != "") {
@@ -45,16 +43,16 @@ function error(&$action) {
       if ($act->Exists($actname,$app->id)) {
 	$act->Set($actname, $app);
 	if ($act->short_name != "")	$action->lay->set("appact", _($act->short_name));
-      } 
-      
-    }
-    
+      }       
+    }    
   } 
   
 
   
   //clear error for next time
-  $action-> ClearError();
+  if (strlen($action->Read("FT_ERROR"))>0) {
+    // $action->ClearError();
+  } 
   
 }
 ?>
