d366237a1b42f6d2225b8174e40c763959a351f7
marc <marc>
2008-08-05 08:17:38 +0000

    Add lang description (flag, label) lang.php

---
+++ App/Core/lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang = array(
+	      
+	      
+	      "fr_FR" => array( "label"  => "français",
+				"flag"   => "", // if don't set to '$code.png'
+				"locale" => "fr" ), 
+
+	      "en_EN" => array( "label"  => "UK english",
+				"locale" => "en" ),
+
+	      "en_US" => array( "label"  => "US english",
+				"locale" => "en" ),
+
+
+	      );
+?>
