ba895317f51508c569b245f3bcc875d40d429719
marc <marc>
2008-08-05 09:01:07 +0000

    lang catalog description

--- App/Core/lang.php
+++ App/Core/lang.php
@@ -3,14 +3,14 @@
 $lang = array(
 	      
 	      
-	      "fr_FR" => array( "label"  => "français",
+	      "fr_FR" => array( "label"  => _("francais"),
 				"flag"   => "", // if don't set to '$code.png'
 				"locale" => "fr" ), 
 
-	      "en_EN" => array( "label"  => "UK english",
+	      "en_EN" => array( "label"  => _("UK english"),
 				"locale" => "en" ),
 
-	      "en_US" => array( "label"  => "US english",
+	      "en_US" => array( "label"  => _("US english"),
 				"locale" => "en" ),
 
 
