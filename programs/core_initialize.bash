7fac66a8b620dab77b4195d83b77b24a5083dce5
Jérôme Augé <jerome.auge@anakeen.com>
2009-06-09 10:01:04 +0200

    Added generation of admin/.htaccess file from admin/.htaccess.in

--- programs/core_initialize.bash
+++ programs/core_initialize.bash
@@ -14,6 +14,8 @@ dbaccess="$WIFF_CONTEXT_ROOT"/context/default/dbaccess.php
 
 prefixtpl="$WIFF_CONTEXT_ROOT"/WHAT/Lib.Prefix.php.in
 prefix="$WIFF_CONTEXT_ROOT"/WHAT/Lib.Prefix.php
+htaccesstpl="$WIFF_CONTEXT_ROOT"/admin/.htaccess.in
+htaccess="$WIFF_CONTEXT_ROOT"/admin/.htaccess
 corepost="$WIFF_CONTEXT_ROOT"/CORE/CORE_post
 
 if [ ! -f "$dbaccesstpl" ]; then
@@ -24,6 +26,10 @@ if [ ! -f "$prefixtpl" ]; then
     echo "file '$prefixtpl' not found" >&2
     exit 1
 fi
+if [ ! -f "$htaccesstpl" ]; then
+    echo "file '$htaccesstpl' not found" >&2
+    exit 1
+fi
 if [ ! -x "$corepost" ]; then
     echo "file '$corepost' not found or not executable" >&2
     exit 1
@@ -31,7 +37,7 @@ fi
 # initialize configuration files
 sed  -e"s;@AUTHTYPE@;$authtype;" -e"s;@CORE_DB@;$core_db;" -e"s;@FREEDOM_DB@;$freedom_db;" "$dbaccesstpl" > "$dbaccess"
 sed  -e"s;@prefix@;$WIFF_CONTEXT_ROOT;" -e"s;@HTTPUSER@;$apacheuser;" "$prefixtpl" > "$prefix"
-
+sed  -e"s;@prefix@;$WIFF_CONTEXT_ROOT;" "$htaccesstpl" > "$htaccess"
 
 export wpub=$WIFF_CONTEXT_ROOT # same as `wiff --getValue=rootdirectory`
 . "$WIFF_CONTEXT_ROOT"/programs/core_environnement
