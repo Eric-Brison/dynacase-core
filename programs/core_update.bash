fb2b08825345861da04983ac3571f8c5017f7b2b
Jérôme Augé <jerome.auge@anakeen.com>
2009-06-16 10:12:12 +0200

    Handle whitespaces in $wpub and dirname

--- programs/core_update.bash
+++ programs/core_update.bash
@@ -2,12 +2,12 @@
 
 corepost=$WIFF_CONTEXT_ROOT"/CORE/CORE_post"
 
-if [ ! -x $corepost ]; then
-    echo "file $corepost not found or not executable" >&2
+if [ ! -x "$corepost" ]; then
+    echo "file '$corepost' not found or not executable" >&2
     exit 1
 fi
 
 export wpub=$WIFF_CONTEXT_ROOT # same as `wiff.php --getValue=rootdirectory`
-. `dirname $0`/core_environment
+. "`dirname $0`"/core_environment
 
-$corepost U
+"$corepost" U
--- programs/update_catalog.bash
+++ programs/update_catalog.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export wpub=$WIFF_CONTEXT_ROOT # same as `wiff.php --getValue=rootdirectory`
-. `dirname $0`/core_environment
-$wpub/whattext
+. "`dirname $0`"/core_environment
+"$wpub/whattext"
 
