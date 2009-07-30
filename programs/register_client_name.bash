c7cc02ce081f21f6ae5f089d22eea009f4c06012
Clément Laballe <clement.laballe@anakeen.com>
2009-07-30 09:54:28 +0200

    Correction for register client name process

--- programs/register_client_name.bash
+++ programs/register_client_name.bash
@@ -2,8 +2,6 @@
 
 client_name = `"$WIFF_ROOT"/wiff --getValue=client_name`
 
-if [ "$client_name" ]; then
+if [ -n "$client_name" ]; then
     "$wpub/wsh.php" --api=set_param --param=CORE_CLIENT --value="$client_name"
-    RET=$?
-    exit $RET
 fi
