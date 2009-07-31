493578fd01862d4453da98fddeceae28c8f5e910
Jérôme Augé <jerome.auge@anakeen.com>
2009-07-31 15:31:28 +0200

    Added removal of dead session files (closes #146).

--- Class/Session/Class.Session.php
+++ Class/Session/Class.Session.php
@@ -263,6 +263,36 @@ Class Session extends DbObj{
     return $res['val'];
   }
   
+  function getSessionMaxAgeSeconds($default="1 week") {
+    $session_maxage = $this->getSessionMaxAge($default);
+    if( preg_match("/^(\d+)\s+(\w+)/i", $session_maxage, $m) ) {
+      $maxage = $m[1];
+      $unit = strtolower($m[2]);
+      switch( substr($unit, 0, 1) ) {
+      case 'y':
+	$maxage = $maxage*364*24*60*60; break; # years
+      case 'm':
+	if( substr($unit, 0, 2) == 'mo' ) {
+	  $maxage = $maxage*30*24*60*60; break; # months
+	} else {
+	  $maxage = $maxage*60; break; # minutes
+	}
+      case 'w':
+	$maxage = $maxage*7*24*60*60; break; # weeks
+      case 'd':
+	$maxage = $maxage*24*60*60; break; # days
+      case 'h':
+	$maxage = $maxage*60*60; break; # hours
+      case 's':
+	break; # seconds
+      default:
+	return FALSE;
+      }
+      return $maxage;
+    }
+    return FALSE;
+  }
+
   function getSessionTTL($default=0) {
     $err = $this->exec_query("SELECT val FROM paramv WHERE name = 'CORE_SESSIONTTL'");
     if( $err != "" ) {
@@ -306,7 +336,56 @@ Class Session extends DbObj{
   
   function deleteExpiredSessions() {
     $err = $this->exec_query("DELETE FROM sessions WHERE last_seen < timestamp 'now()' - interval '".pg_escape_string($this->getSessionMaxAge())."'");
-    return $err;
+    if( $err != "" ) {
+      return $err;
+    }
+
+    return "";
+  }
+
+  function deleteExpiredSessionFiles() {
+    include_once('Lib.Prefix.php');
+
+    global $pubdir;
+
+    $session_maxage = $this->getSessionMaxAgeSeconds();
+    if( $session_maxage === false ) {
+      $err = sprintf("Malformed CORE_SESSIONMAXAGE");
+      return $err;
+    }
+    $maxage = time() - $session_maxage;
+
+    $sessionDir = sprintf("%s/session", $pubdir);
+    $dir = opendir($sessionDir);
+    if( $dir === false ) {
+      $err = sprintf("Error opening directory '%s'.", $sessionDir);
+      return $err;
+    }
+
+    $sessions = array();
+    while( $file = readdir($dir) ) {
+      if( preg_match("/^sess_(.+)$/", $file, $m) ) {
+	$sessions[$m[1]] = sprintf("%s/%s", $sessionDir, $file);
+      }
+    }
+    closedir($dir);
+
+    if( count(array_keys($sessions)) <= 0 ) {
+      return "";
+    }
+
+    foreach( $sessions as $sess_id => $sess_file ) {
+      $stat = @stat($sess_file);
+      if( $stat === false ) {
+	$err = sprintf("Error stat(%s).", $sess_file);
+	return $err;
+      }
+      if( $stat['mtime'] < $maxage ) {
+	@unlink($sess_file);
+      }
+    }
+
+    return "";
   }
   
   function gcSessions() {
@@ -319,6 +398,11 @@ Class Session extends DbObj{
       $err = $this->deleteExpiredSessions();
       if( $err != "" ) {
 	error_log(__CLASS__."::".__FUNCTION__." "."Error cleaning up sessions : ".$err);
+	return $err;
+      }
+      $err = $this->deleteExpiredSessionFiles();
+      if( $err != "" ) {
+	error_log(__CLASS__."::".__FUNCTION__." "."Error cleaning up session files : ".$err);
 	return $err;
       }
     }
