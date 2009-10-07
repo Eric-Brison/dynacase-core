4f1d08f7015f7af65e27738c5545f53a5aa318b0
Clément Laballe <clement.laballe@anakeen.com>
2009-10-07 11:03:52 +0200

    Error in record_application [closes #378]

--- programs/record_application.bash
+++ programs/record_application.bash
@@ -4,9 +4,12 @@ export wpub=$WIFF_CONTEXT_ROOT # same as `wiff.php --getValue=rootdirectory`
 . "`dirname $0`"/core_environment
 
 if [ -z "$2" ]; then
-    "$wpub/wsh.php"  --api=appadmin --method=update --appname="$1"
-    RET=$?
-    exit $RET
+	"$wpub/wsh.php" --api=appadmin --method=update --appname="$1"
+	RET=$?
+	if [ $RET -ne 0 ]; then
+	echo "Error: appadmin update of '$1' returned with exit code '$RET'"
+	exit $RET
+	fi
 fi
 
 if [ "$2" = "I" ]; then
