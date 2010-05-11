4f845d71bb161eef6b8cc0c17c5566722ef6baf0
Eric Brison <eric.brison@anakeen.com>
2010-05-11 10:12:03 +0200

    correct window ext open (refs #681)

--- Share/subwindow.js
+++ Share/subwindow.js
@@ -7,54 +7,53 @@
 
 
 function subwindow(h, w, name, url) {
-	
-	if(window.parent.Ext){
-		window.parent.Ext.fdl.Interface.prototype.publish('openurl',url,name);
-		var me = windowExist(name, true);
-		if (me && (!me.opener)) {
-			me.opener=window;
-			if (! me.opener) me._opener=window;
-			console.log('add opener ME',me._opener);
-		}
-		console.log('subwindow ME',me);
-		return me;
+
+	var me = windowExist(name, true);
+
+	if (! me) {
+		if(window.parent.Ext){
+			window.parent.Ext.fdl.Interface.prototype.publish('openurl','about:blank',name);
+			me  = window.open(url,name);
+			
+			return me;
+		} else {
+
+			var screen_width, screen_height;
+			var win_top, win_left;
+			var HelpWin;
+			screen_height        = 0;     screen_width      = 0;
+			win_top              = 0;     win_left          = 0;
+			if (window.innerWidth) screen_width = window.innerWidth;
+			if (window.innerHeight) screen_height = window.innerHeight;
+			win_top  = screen_height - h - 20;
+			win_left = screen_width  - w  - 20;
+
+
+
+			me  = window.open(
+					url,
+					name,
+					'resizable=yes,scrollbars=yes,width='+w+',height='+h+',top='+win_top+',left='+win_left);
+			if (!me) {
+				if (confirm("Ouverture fenêtre impossible.\nVoulez vous affichez la page dans la fenêtre courante ?")) {
+					window.location.href=url;
+				}
+			} else {
+				getConnexeWindows(me);
+			}
+		} 
 	} else {
-		
-   var screen_width, screen_height;
-   var win_top, win_left;
-   var HelpWin;
-   screen_height        = 0;     screen_width      = 0;
-   win_top              = 0;     win_left          = 0;
-   if (window.innerWidth) screen_width = window.innerWidth;
-   if (window.innerHeight) screen_height = window.innerHeight;
-   win_top  = screen_height - h - 20;
-   win_left = screen_width  - w  - 20;
-   var me = windowExist(name, true);
-  
-   if (! me) {
-     me  = window.open(
-		       url,
-		       name,
-		       'resizable=yes,scrollbars=yes,width='+w+',height='+h+',top='+win_top+',left='+win_left);
-     if (!me) {
-       if (confirm("Ouverture fenêtre impossible.\nVoulez vous affichez la page dans la fenêtre courante ?")) {
-	 window.location.href=url;
-       }
-     } else {
-       getConnexeWindows(me);
-     }
-   } else {
-     me.location.href=url;
-   }
-  if (me) {
-	  me.focus();
-  }
-  
-  return me;
-  
+		me.location.href=url;
 	}
-  
+	if (me) {
+		me.focus();
+	}
+
+	return me;
+
 }
+
+
 
 // with menu
 function subwindowm(h, w, name, url) {
