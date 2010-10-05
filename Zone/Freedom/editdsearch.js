47313cf4e1b4ac797fcde9c1ba01c2bb28b4f099
Arnaud <arnaud@arnaud-laptop.(none)>
2010-10-05 14:12:10 +0200

    add autocompletion on docids (closes #1236)

--- Zone/Freedom/editdsearch.js
+++ Zone/Freedom/editdsearch.js
@@ -7,163 +7,212 @@
 
 // use when submit to avoid first unused item
 function deletenew() {
-  if (canmodify(true)) {
-    resetInputs('newcond');
-    var na=document.getElementById('newcond');
-    if (na) na.parentNode.removeChild(na); 
-    na=document.getElementById('newstate');
-    if (na) na.parentNode.removeChild(na);
-  }
+	if (canmodify(true)) {
+		resetInputs('newcond');
+		var na=document.getElementById('newcond');
+		if (na) na.parentNode.removeChild(na);
+		na=document.getElementById('newstate');
+		if (na) na.parentNode.removeChild(na);
+	}
   
 }
   
 
 function sendsearch(faction,artarget) {
-  var fedit = document.fedit;
-  resetInputs('newcond');
+	var fedit = document.fedit;
+	resetInputs('newcond');
   
-  with (document.modifydoc) {
-    var editAction=action;
-    var editTarget=target;
+	with (document.modifydoc) {
+		var editAction=action;
+		var editTarget=target;
 
-    enableall();  
-    var na=document.getElementById('newcond');
-    if (na) {
-      disabledInput(na,true);        
-      var nt=document.getElementById('newstate');
-      if (nt)   disabledInput(nt,true);
-    }
-    if ((!artarget) &&  (window.parent.fvfolder)) artarget='fvfolder';
-    else if ((!artarget) &&  (window.parent.flist)) {
-      artarget='flist';
-      faction=faction + '&ingeneric=yes';
-    } else  if (!artarget) artarget='_blank';
-    target=artarget;
-    action=faction;
-    submit();
-    target=editTarget;
-    action=editAction;
+		enableall();
+		var na=document.getElementById('newcond');
+		if (na) {
+			disabledInput(na,true);
+			var nt=document.getElementById('newstate');
+			if (nt)   disabledInput(nt,true);
+		}
+		if ((!artarget) &&  (window.parent.fvfolder)) artarget='fvfolder';
+		else if ((!artarget) &&  (window.parent.flist)) {
+			artarget='flist';
+			faction=faction + '&ingeneric=yes';
+		} else  if (!artarget) artarget='_blank';
+		target=artarget;
+		action=faction;
+		submit();
+		target=editTarget;
+		action=editAction;
 
     
-    if (na) {
-      disabledInput(na,false);            
-       if (nt) disabledInput(nt,false);
-    }
+		if (na) {
+			disabledInput(na,false);
+			if (nt) disabledInput(nt,false);
+		}
     
-  }
+		}
 }
 function callFunction(event,th) {
-  var pnode=getPrevElement(th.parentNode);
-  var ex=document.getElementById('example');
-  if (pnode) {
-    pnode.innerHTML='<input  type="text"  size="20" name="_se_keys[]">';
-    pnode.appendChild(ex);
-    ex.style.display='';
-  }
+	var pnode=getPrevElement(th.parentNode);
+	var ex=document.getElementById('example');
+	if (pnode) {
+		pnode.innerHTML='<input  type="text"  size="20" name="_se_keys[]">';
+		pnode.appendChild(ex);
+		ex.style.display='';
+	}
   
 }
 function setKey(event,th) {
-  var pnode;
+	var pnode;
 
-  pnode=th.previousSibling;
-  while (pnode && ((pnode.nodeType != 1) || (pnode.name != '_se_keys[]'))) pnode = pnode.previousSibling;
+	pnode=th.previousSibling;
+	while (pnode && ((pnode.nodeType != 1) || (pnode.name != '_se_keys[]'))) pnode = pnode.previousSibling;
 
-  pnode.value = th.options[th.selectedIndex].value;
+	pnode.value = th.options[th.selectedIndex].value;
 
   
 }
 
 function getNextElement(th) {
-  var pnode;
-  pnode=th.nextSibling;
-  while (pnode && (pnode.nodeType != 1)) pnode = pnode.nextSibling;
-  return pnode;
+	var pnode;
+	pnode=th.nextSibling;
+	while (pnode && (pnode.nodeType != 1)) pnode = pnode.nextSibling;
+	return pnode;
   
 }
 
 function getPrevElement(th) {
-  var pnode;
-  pnode=th.previousSibling;
-  while (pnode && (pnode.nodeType != 1)) pnode = pnode.previousSibling;
-  return pnode;
+	var pnode;
+	pnode=th.previousSibling;
+	while (pnode && (pnode.nodeType != 1)) pnode = pnode.previousSibling;
+	return pnode;
   
 }
 
+function filterfunc2(th) {
+	var so, i;
+	var pnode = th.parentNode.previousSibling;
+	while (pnode && ((pnode.nodeType != 1) || (pnode.tagName != 'TD'))) pnode = pnode.previousSibling;
+	for (i=0;i<pnode.childNodes.length;i++) {
+		if (pnode.childNodes[i].tagName=='SELECT') {
+			so=pnode.childNodes[i];
+		}
+	}
+	if(so) {
+		filterfunc(so);
+	}
+}
+
 function filterfunc(th) {
-  var p=th.parentNode;
-  var opt=th.options[th.selectedIndex];
-  var atype=opt.getAttribute('atype');
-  var ctypes,i;
-  var pnode,so=false;
-  var aid=opt.value;
-  var sec,se;
-  var needresetselect=false,ifirst=0;
-  var ex=document.getElementById('example');
-  var lc=document.getElementById('lastcell');
+	var p=th.parentNode;
+	var opt=th.options[th.selectedIndex];
+	var atype=opt.getAttribute('atype');
+	var ctypes,i;
+	var pnode,so=false;
+	var aid=opt.value;
+	var sec,se;
+	var needresetselect=false,ifirst=0;
+	var ex=document.getElementById('example');
+	var lc=document.getElementById('lastcell');
 
-  // move to tfoot to not be removed
-  if (ex)  {
-    ex.style.display='none';
-    lc.appendChild(ex);
-  }
+	// move to tfoot to not be removed
+	if (ex)  {
+		ex.style.display='none';
+		lc.appendChild(ex);
+	}
 
-  // search brother select input
-  pnode=p.nextSibling;
-  while (pnode && ((pnode.nodeType != 1) || (pnode.tagName != 'TD'))) pnode = pnode.nextSibling;
+	// search brother select input
+	pnode=p.nextSibling;
+	while (pnode && ((pnode.nodeType != 1) || (pnode.tagName != 'TD'))) pnode = pnode.nextSibling;
 
  
-  for (i=0;i<pnode.childNodes.length;i++) {
-    if (pnode.childNodes[i].tagName=='SELECT') {
-      so=pnode.childNodes[i];
-    }
-  }
-
-  // display only matches
-  ifirst=-1;
-  for (i=0;i<so.options.length;i++) {
-    opt=so.options[i];
-    ctype=opt.getAttribute('ctype');
-    if ((ctype=='') || (ctype.indexOf(atype)>=0)) {
-      if (ifirst == -1) ifirst=i;
-      opt.style.display='';
-      opt.disabled=false;
-    } else {
-      opt.style.display='none';
-      if (opt.selected) needresetselect=true;
-      opt.selected=false;
-      opt.disabled=true;
-    }
-  }
-  if (needresetselect) {
-    so.options[ifirst].selected=true;
-  }
-
-  // find key cell
-  pnode=pnode.nextSibling;
-  while (pnode && ((pnode.nodeType != 1) || (pnode.tagName != 'TD'))) pnode = pnode.nextSibling;
-  // now enum
-  if ((atype=='enum') || (atype=='enumlist')) {   
-     se=document.getElementById('selenum'+aid);
-    if (se) {      
-      if (pnode) {	
-	pnode.innerHTML='';
-	sec=se.cloneNode(true);
-	sec.name='_se_keys[]';
-	sec.id='';
-	pnode.appendChild(sec);
-      }            
-    }
-  } else {    
-    se=document.getElementById('thekey');
-    if (se) {            
-	sec=se.cloneNode(true);
-	sec.name='_se_keys[]';
-	sec.id='';
-	if (pnode) {            
-	  pnode.innerHTML='';
-	  pnode.appendChild(sec);	  
+	for (i=0;i<pnode.childNodes.length;i++) {
+		if (pnode.childNodes[i].tagName=='SELECT') {
+			so=pnode.childNodes[i];
+		}
 	}
-    }
-  }
+
+
+	// display only matches
+	ifirst=-1;
+	for (i=0;i<so.options.length;i++) {
+		opt=so.options[i];
+		ctype=opt.getAttribute('ctype');
+		if ((ctype=='') || (ctype.indexOf(atype)>=0)) {
+			if (ifirst == -1) ifirst=i;
+			opt.style.display='';
+			opt.disabled=false;
+		} else {
+			opt.style.display='none';
+			if (opt.selected) needresetselect=true;
+			opt.selected=false;
+			opt.disabled=true;
+		}
+	}
+	if (needresetselect) {
+		so.options[ifirst].selected=true;
+	}
+	var egaloperator = false;
+	if(so.value == '=' || so.value == '!=') {
+		egaloperator = true;
+	}
+
+
+	// find key cell
+	pnode=pnode.nextSibling;
+	while (pnode && ((pnode.nodeType != 1) || (pnode.tagName != 'TD'))) pnode = pnode.nextSibling;
+	// now enum
+	if ((atype=='enum') || (atype=='enumlist')) {
+		se=document.getElementById('selenum'+aid);
+		if (se && pnode) {
+			pnode.innerHTML='';
+			sec=se.cloneNode(true);
+			sec.name='_se_keys[]';
+			sec.id='';
+			pnode.appendChild(sec);
+		}
+	} else if(atype == 'docid') {
+		se=document.getElementById('thekey');
+		if (se && pnode) {
+			if(!egaloperator) {
+				sec=se.cloneNode(true);
+				sec.name='_se_keys[]';
+				sec.id='';
+				pnode.innerHTML='';
+				pnode.appendChild(sec);
+			}
+			else {
+				var loc = window.location.href;
+				var famid, m;
+				if(m = loc.match(/&sfamid=([^&]+)/)) {
+					famid = m[1];
+				}
+				else if(m = loc.match(/&classid=([^&]+)/)) {
+					famid = m[1];
+				}
+				if(famid) {
+					var html = '<input type="hidden"  name="_se_keys[]" id="'+aid+'" value="">';
+					html += '<input autocomplete="off" autoinput="1" onfocus="activeAuto(event,'+famid+',this,\'\',\''+aid+'\',\'\')"   onchange="addmdocs(\'_'+aid+'\')" type="text" name="_ilink_'+aid+'" id="ilink_'+aid+'" value="">';
+					pnode.innerHTML= html;
+				}
+				else {
+					sec=se.cloneNode(true);
+					sec.name='_se_keys[]';
+					sec.id='';
+					pnode.innerHTML='';
+					pnode.appendChild(sec);
+				}
+			}
+		}
+	} else {
+		se=document.getElementById('thekey');
+		if (se && pnode) {
+			sec=se.cloneNode(true);
+			sec.name='_se_keys[]';
+			sec.id='';
+			pnode.innerHTML='';
+			pnode.appendChild(sec);
+		}
+	}
   
 }
