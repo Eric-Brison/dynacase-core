eb59b786e33eb0d445b923593c59bfb016c5c084
Jérôme Augé <jerome.auge@anakeen.com>
2010-11-26 17:25:43 +0100

    Fix postgresql warnings with nonstandard escape in string literal (closes #945)

--- App/Fdl/fdl.sql
+++ App/Fdl/fdl.sql
@@ -1,5 +1,5 @@
 create or replace function in_textlist(text, text) 
-returns bool as '
+returns bool as $$
 declare 
   arg_tl alias for $1;
   arg_v alias for $2;
@@ -10,25 +10,25 @@ begin
   if (not rvalue) then	
     
      -- search in middle
-    wt := ''\n''||arg_v||''\n'';
+    wt := E'\n'||arg_v||E'\n';
     rvalue := (position(wt in arg_tl) > 0);
 
      -- search in begin
      if (not rvalue) then	
-       wt := arg_v||''\n'';
+       wt := arg_v||E'\n';
        rvalue := (position(wt in arg_tl) = 1);
 
 	
         -- search in end
        if (not rvalue) then	
-          wt := ''\n''||arg_v;
+          wt := E'\n'||arg_v;
           rvalue := (position(wt in arg_tl) = (char_length(arg_tl)-char_length(arg_v))) and (position(wt in arg_tl) > 0);	
         end if;
      end if;
   end if;
   return rvalue;
 end;
-' language 'plpgsql';
+$$ language 'plpgsql';
 
 
 
