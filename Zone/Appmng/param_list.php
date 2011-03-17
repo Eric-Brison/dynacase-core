3e9718f792cf4fe7751640f229b923f030976028
Eric Brison <eric.brison@anakeen.com>
2011-03-17 11:55:30 +0100

    use stars when view password in parameters interface (closes #1574)

--- Zone/Appmng/param_list.php
+++ Zone/Appmng/param_list.php
@@ -8,142 +8,135 @@
  * @package WHAT
  * @subpackage APPMNG
  */
- /**
+/**
  */
 
-include_once("Class.TableLayout.php");
-include_once("Class.QueryDb.php");
-include_once("Class.Param.php");
-include_once("Class.SubForm.php");
+include_once ("Class.TableLayout.php");
+include_once ("Class.QueryDb.php");
+include_once ("Class.Param.php");
+include_once ("Class.SubForm.php");
 // -----------------------------------
-function param_list(&$action) {
-  // -----------------------------------
+function param_list(&$action)
+{
+    // -----------------------------------
+    
 
     // Get Param
+    
 
-  $userid=GetHttpVars("userid");
-  $styleid=GetHttpVars("styleid");
-  $pview=GetHttpVars("pview"); // set to "all" or "single" if user parameters
-
+    $userid = GetHttpVars("userid");
+    $styleid = GetHttpVars("styleid");
+    $pview = GetHttpVars("pview"); // set to "all" or "single" if user parameters
+    
 
     // Set the globals elements
-      $baseurl=$action->GetParam("CORE_BASEURL");
-  $standurl=$action->GetParam("CORE_STANDURL");
-
-  
-  
-  
-  
-  
-  $action->parent->AddJsRef($action->GetParam("CORE_JSURL")."/subwindow.js");
-  $action->parent->AddJsRef($action->GetParam("CORE_JSURL")."/AnchorPosition.js");
-  $action->parent->AddJsRef($action->GetParam("CORE_JSURL")."/PopupWindow.js");
-  $action->parent->AddJsRef($action->GetParam("CORE_JSURL")."/ColorPicker2.js");
-  $action->parent->AddJsRef($action->GetParam("CORE_JSURL")."/OptionPicker.js");
-  
-  
-  $tincparam=array();
-  $appinc=array();
-  
-
-
-
+    $baseurl = $action->GetParam("CORE_BASEURL");
+    $standurl = $action->GetParam("CORE_STANDURL");
+    
+    $action->parent->AddJsRef($action->GetParam("CORE_JSURL") . "/subwindow.js");
+    $action->parent->AddJsRef($action->GetParam("CORE_JSURL") . "/AnchorPosition.js");
+    $action->parent->AddJsRef($action->GetParam("CORE_JSURL") . "/PopupWindow.js");
+    $action->parent->AddJsRef($action->GetParam("CORE_JSURL") . "/ColorPicker2.js");
+    $action->parent->AddJsRef($action->GetParam("CORE_JSURL") . "/OptionPicker.js");
+    
+    $tincparam = array();
+    $appinc = array();
+    
     switch ($pview) {
-    case "allapp":
-      $tparam = $action->parent->param->GetApps();
-      break;
-    case "alluser":
-      if ($userid == "") $tparam = array();
-      else $tparam = $action->parent->param->GetUser($userid);
-      uasort($tparam,"cmpappid");
-      break;
-    case "singleuser":
-      if ($userid == "") $tparam = array();
-      else $tparam = $action->parent->param->GetUser($userid,$action->getParam("STYLE"));
-      uasort($tparam,"cmpappid");
-      break;
-    case "allstyle":
-      if ($styleid == "") $tparam = array();
-      else $tparam = $action->parent->param->GetStyle($styleid);
-      break;
+    case "allapp" :
+        $tparam = $action->parent->param->GetApps();
+        break;
+    case "alluser" :
+        if ($userid == "") $tparam = array();
+        else $tparam = $action->parent->param->GetUser($userid);
+        uasort($tparam, "cmpappid");
+        break;
+    case "singleuser" :
+        if ($userid == "") $tparam = array();
+        else $tparam = $action->parent->param->GetUser($userid, $action->getParam("STYLE"));
+        uasort($tparam, "cmpappid");
+        break;
+    case "allstyle" :
+        if ($styleid == "") $tparam = array();
+        else $tparam = $action->parent->param->GetStyle($styleid);
+        break;
     }
     
+    $vsection = "appid";
     
-    
-    
-      $vsection="appid";
-    
-    $precApp=0;
-  $tincparam=array();
-  $applist = "";
-    while (list($k,$v)= each ($tparam)) {
-      if (isset($v[$vsection])) {
-	if ($v[$vsection] != $precApp) {
-	  
-	  $action->lay->SetBlockData("PARAM$precApp",$tincparam);
-	  $tincparam=array();
-	  $precApp = $v[$vsection];
-	  
-	  $app1=new Application($action->dbaccess,$precApp);
-
-
-	  $appinc[$precApp]["appname"]=$app1->name;
-	  $appinc[$precApp]["appicon"]=$action->getImageUrl($app1->icon);
-          $applist .= ($applist==""?"":",");
-          $applist .= "'".$app1->name."'";
-	  $appinc[$precApp]["appdesc"]=$action->text($app1->short_name);
-	  $appinc[$precApp]["PARAM"]="PARAM$precApp";
-          
-	}
-	$tincparam[$k]=$v;
-	// to show difference between global, user and application parameters
-	  if ($v["type"][0] == PARAM_APP) $tincparam[$k]["classtype"]="aparam";
-	  else if ($v["type"][0] == PARAM_USER) $tincparam[$k]["classtype"]="uparam";
-	  else if ($v["type"][0] == PARAM_STYLE) $tincparam[$k]["classtype"]="sparam";
-	  else $tincparam[$k]["classtype"]="gparam";
-	$tincparam[$k]["sval"]=str_replace(array('"'),array("&quot;"),$v["val"]);
-	
-	$tincparam[$k]["colorstatic"]=($v["kind"]=="static")?"#666666":"";
-
-	// force type user if user mode
-	  if ($userid > 0) $tincparam[$k]["type"]=PARAM_USER.$userid;
-	else if ($styleid != "") $tincparam[$k]["type"]=PARAM_STYLE.$styleid;
-
-	if ($tincparam[$k]["descr"]=="") $tincparam[$k]["descr"]=$tincparam[$k]["name"];
-	else $tincparam[$k]["descr"]=_($tincparam[$k]["descr"]);
-	$tincparam[$k]["tooltip"] = $tincparam[$k]["name"] . " : " .$tincparam[$k]["descr"];
-
-      }
+    $precApp = 0;
+    $tincparam = array();
+    $applist = "";
+    foreach ( $tparam as $k => $v ) {
+        if (isset($v[$vsection])) {
+            if ($v[$vsection] != $precApp) {
+                
+                $action->lay->SetBlockData("PARAM$precApp", $tincparam);
+                $tincparam = array();
+                $precApp = $v[$vsection];
+                
+                $app1 = new Application($action->dbaccess, $precApp);
+                
+                $appinc[$precApp]["appname"] = $app1->name;
+                $appinc[$precApp]["appicon"] = $action->getImageUrl($app1->icon);
+                $applist .= ($applist == "" ? "" : ",");
+                $applist .= "'" . $app1->name . "'";
+                $appinc[$precApp]["appdesc"] = $action->text($app1->short_name);
+                $appinc[$precApp]["PARAM"] = "PARAM$precApp";
+            
+            }
+            $tincparam[$k] = $v;
+            // to show difference between global, user and application parameters
+            if ($v["type"][0] == PARAM_APP) $tincparam[$k]["classtype"] = "aparam";
+            else if ($v["type"][0] == PARAM_USER) $tincparam[$k]["classtype"] = "uparam";
+            else if ($v["type"][0] == PARAM_STYLE) $tincparam[$k]["classtype"] = "sparam";
+            else $tincparam[$k]["classtype"] = "gparam";
+            if ($v["kind"] == "password") {
+                if ($v["val"]!="") $v["val"]="*****";
+                $tincparam[$k]["val"]=$v["val"];
+            }
+            $tincparam[$k]["sval"] = str_replace(array('"'), array("&quot;"), $v["val"]);
+            
+            $tincparam[$k]["colorstatic"] = ($v["kind"] == "static") ? "#666666" : "";
+            
+            // force type user if user mode
+            if ($userid > 0) $tincparam[$k]["type"] = PARAM_USER . $userid;
+            else if ($styleid != "") $tincparam[$k]["type"] = PARAM_STYLE . $styleid;
+            
+            if ($tincparam[$k]["descr"] == "") $tincparam[$k]["descr"] = $tincparam[$k]["name"];
+            else $tincparam[$k]["descr"] = _($tincparam[$k]["descr"]);
+            $tincparam[$k]["tooltip"] = $tincparam[$k]["name"] . " : " . $tincparam[$k]["descr"];
+        
+        }
     }
     
-    $action->lay->SetBlockData("PARAM$precApp",$tincparam);
+    $action->lay->SetBlockData("PARAM$precApp", $tincparam);
     if ($pview == "singleuser") { // chg action because of acl USER/ADMIN
-      $action->lay->Set("ACTIONDEL","PARAM_UDELETE");     
-      $action->lay->Set("ACTIONMOD","PARAM_UMOD");     
+        $action->lay->Set("ACTIONDEL", "PARAM_UDELETE");
+        $action->lay->Set("ACTIONMOD", "PARAM_UMOD");
     } else {
-      $action->lay->Set("ACTIONDEL","PARAM_DELETE");    
-      $action->lay->Set("ACTIONMOD","PARAM_MOD");     
+        $action->lay->Set("ACTIONDEL", "PARAM_DELETE");
+        $action->lay->Set("ACTIONMOD", "PARAM_MOD");
     }
+    
+    uasort($appinc, "cmpappname");
+    $action->lay->set("AppList", $applist);
+    $action->lay->SetBlockData("APPLI", $appinc);
 
-  
-
-  uasort($appinc,"cmpappname");
-  $action->lay->set("AppList",$applist);
-  $action->lay->SetBlockData("APPLI",$appinc);
-  
-  
 }
-function cmpappid($a, $b) {
-  if ($a["appid"] == $b["appid"]) return 0;
-  if ($a["appid"] > $b["appid"]) return 1;
-  return -1;
-  
+function cmpappid($a, $b)
+{
+    if ($a["appid"] == $b["appid"]) return 0;
+    if ($a["appid"] > $b["appid"]) return 1;
+    return -1;
+
 }
 
-function cmpappname($a, $b) {
-  if ($a["appname"] == $b["appname"]) return 0;
-  if ($a["appname"] > $b["appname"]) return 1;
-  return -1;
-  
+function cmpappname($a, $b)
+{
+    if ($a["appname"] == $b["appname"]) return 0;
+    if ($a["appname"] > $b["appname"]) return 1;
+    return -1;
+
 }
 ?>
