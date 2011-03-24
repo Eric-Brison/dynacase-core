a41aec63f29b31e0627becb9454a9793ee959d6d
Jérôme Augé <jerome.auge@anakeen.com>
2011-03-24 18:19:54 +0100

    Removed old migration scripts < 2.14 (refs #1098)

--- App/Usercard/USERCARD_migr_0.2.5.sql
+++
@@ -1,9 +0,0 @@
-alter table doc120 add column us_role text;
-alter table doc120 add column us_scatg text;
-
-update doc120 set us_role=us_type;
-update doc120 set us_scatg=us_catg;
-update docattr set phpfunc=(select phpfunc from docattr where docid=120 and id='us_catg') where docid=124 and id='si_catg';
-
-delete from docattr where id='us_catg';
---alter table doc120 drop column us_catg;
