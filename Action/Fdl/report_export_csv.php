9f1a9dbb30d439589466f2b6290fa073782c0067
Charles Bonnissent <charles.bonnissent@anakeen.com>
2011-05-16 17:55:31 +0200

    Update csv export to handle http action and have an IHM refs : #1538

--- Action/Fdl/report_export_csv.php
+++ Action/Fdl/report_export_csv.php
@@ -1,6 +1,8 @@
 <?php
 
 require_once 'FDL/Class.Doc.php';
+require_once 'FDL/freedom_util.php';
+require_once 'EXTERNALS/fdl.php';
 
 function usage()
 {
@@ -11,49 +13,83 @@ function usage()
     return $usageString;
 }
 
+function report_export_csv(&$action)
+{
+    $dbaccess = getParam('FREEDOM_DB');
 
-function report_export_csv(&$action) {
-$dbaccess = getParam('FREEDOM_DB');
+    $id = $action->getArgument('id', false);
+    $kind = $action->getArgument('kind', "simple");
+    $pivot = $action->getArgument('pivot', false);
+    $refresh = $action->getArgument('refresh', false);
+    $delimiter = $action->getArgument('delimiter', ",");
+    $enclosure = $action->getArgument('enclosure', '"');
+    $encoding = $action->getArgument('encoding', 'utf8');
+    $decimalSeparator = $action->getArgument('decimalSeparator', ".");
+    $displayForm = $action->getArgument('displayForm', false);
 
-$id = $action->getArgument('id', false);
-$pivot = $action->getArgument('pivot', false);
-$refresh = $action->getArgument('refresh', false);
-$decimalSeparator = $action->getArgument('decimalSeparator', ".");
+    if (!$id) {
+        $action->exitError(usage() . 'no report id given');
+    }
 
-if (!$id) {
-    $action->exitError(usage().'no report id given');
+    $currentDoc = new_Doc($dbaccess, $id);
+
+    if ($displayForm) {
+        $action->lay->set("id", $id);
+        $attr = getDocAttr($dbaccess, $currentDoc->getValue("se_famid", 1));
+        $attributeLay = array();
+        $attributeLay[]= array("key" => "id", "libelle" => _("EXPORT_CSV : identifiant unique"));
+        foreach ($attr as $currentAttr) {
+            $attributeLay[]= array("key" => $currentAttr[1], "libelle" => $currentAttr[0]);
+        }
+        $action->lay->setBlockData("pivotAttribute", $attributeLay);
+    }else {
+
+        $reportFamId = getIdFromName($dbaccess, "REPORT");
+        $familyIdArray = $currentDoc->getFromDoc();
+
+        if (in_array($reportFamId, $familyIdArray)) {
+
+            switch ($kind) {
+            case "pivot":
+                $csvStruct = $currentDoc->generateCSVReportStruct($decimalSeparator, $refresh, true, $pivot);
+                break;
+            default:
+                $csvStruct = $currentDoc->generateCSVReportStruct($decimalSeparator, $refresh, false);
+            }
+
+            $csvFile = tempnam($action->GetParam("CORE_TMPDIR", "/tmp"), "csv$id") . ".csv";
+            $fp = fopen($csvFile, 'w');
+
+            foreach ( $csvStruct as $currentLine ) {
+                if ($encoding != "utf8") {
+                    $currentLine = convertLine($currentLine, $encoding);
+                }
+                fputcsv($fp, $currentLine, $delimiter, $enclosure);
+            }
+
+            fclose($fp);
+
+            if (!$_SERVER['HTTP_HOST']) {
+                print $csvFile . "\n";
+            }else {
+                $fileName = sprintf("%s_%s.%s", $currentDoc->getTitle(), date("Y_m_d-H_m_s"), "csv");
+                 Http_DownloadFile($csvFile, $fileName , "text/csv", false, false, true);
+            }
+
+            exit(0);
+
+        } else {
+            $action->exitError('The document is not a report');
+        }
+    }
+
 }
 
-$currentDoc = new_Doc($dbaccess, $id);
-
-$reportFamId = getIdFromName($dbaccess, "REPORT");
-
-$familyIdArray = $currentDoc->getFromDoc();
-
-if (in_array($reportFamId, $familyIdArray)) {
-
-        if ($pivot) {
-            $csvStruct = $currentDoc->generateCSVReportStruct($decimalSeparator ,$refresh, true, $pivot);
-        }else {
-            $csvStruct = $currentDoc->generateCSVReportStruct($decimalSeparator ,$refresh, false);
-        }
-
-        $csvFile = tempnam($action->GetParam("CORE_TMPDIR", "/tmp"), "csv$id").".csv";
-
-        $fp = fopen($csvFile, 'w');
-
-        foreach ( $csvStruct as $currentLine ) {
-            fputcsv($fp, $currentLine);
-        }
-
-        fclose($fp);
-
-        print $csvFile."\n";
-
-        exit(0);
-
-}else {
-    $action->exitError('The document is not a report');
-}
-
+function convertLine($currentLine, $encoding)
+{
+    $returnArray = array();
+    foreach ( $currentLine as $currentValue ) {
+        $returnArray[] = iconv("utf8", $encoding, $currentValue);
+    }
+    return $returnArray;
 }
