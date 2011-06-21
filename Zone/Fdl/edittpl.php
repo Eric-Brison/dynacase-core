27bdb898268edc23bd2c6fd597a3596dfd9b92e4
Charles Bonnissent <charles.bonnissent@anakeen.com>
2011-06-21 12:40:00 +0200

    Correct special edit template for an attribute
    Use the same code than view template
    closes #1711

--- Zone/Fdl/edittpl.php
+++ Zone/Fdl/edittpl.php
@@ -17,34 +17,46 @@ include_once("FDL/Class.Dir.php");
 
 
 /**
- * View a document
+ * Edit special template for an attribute
+ * 
  * @param Action &$action current action
  * @global id Http var : document identificator to see
  * @global famid Http var : 
  * @global zone Http var : zone 
  */
 function edittpl(Action &$action) {
-    $zone=$action->getArgument("zone");
-    $docid=$action->getArgument("id");
-    if (! $docid) $docid=$action->getArgument("famid");
-    if (! $zone) $action->addWarningMsg(_("no template defined"));
-    if (! $docid) $action->addWarningMsg(sprintf(_("template %s no document found"),$zone));
-    $dbaccess=$action->getParam("FREEDOM_DB");
-
-    if (preg_match("/([^:]+):([^:]+)/",$zone, $reg)) {
-        $appname=$reg[1];
-        $doc=new_doc($dbaccess,$docid);
-        if ($doc->isAlive()) {
-            $zopt=$doc->getZoneOption($zone);
-            $withtd=($zopt=="U");
-            $action->lay=new Layout($doc->getZoneFile($zone),$action);
-            $doc->lay=&$action->lay;
-            $doc->editattr($withtd);
+    $zone = $action->getArgument("zone");
+    $docid = $action->getArgument("id");
+    if (!$docid)
+        $docid = $action->getArgument("famid");
+    if (!$zone)
+        $action->addWarningMsg(_("no template defined"));
+    if (!$docid)
+        $action->addWarningMsg(sprintf(_("template %s no document found"), $zone));
+    $dbaccess = $action->getParam("FREEDOM_DB");
+    
+    $reg = Doc::parseZone($zone);
+    if ($reg === false) {
+        return sprintf(_("error in pzone format %s"), $zone);
+    }
+    
+    if (array_key_exists('argv', $reg)) {
+        foreach ( $reg['argv'] as $k => $v ) {
+            setHttpVar($k, $v);
+        }
+    }
+    
+    $doc = new_doc($dbaccess, $docid);
+    if ($doc->isAlive()) {
+        $action->lay = new Layout($doc->getZoneFile($zone), $action);
+        $doc->lay = &$action->lay;
+        
+        $method = strtok(strtolower($reg['layout']), '.');
+        if (method_exists($doc, $method)) {
+            $doc->$method();
+        } else {
+            $doc->editattr($doc->getZoneOption($zone)=="U");
             $doc->viewprop();
-            $method = strtok(strtolower($zone),'.');
-            if (method_exists ( $doc, $method)) {
-                $doc->$method();
-            }
         }
     }
 }
