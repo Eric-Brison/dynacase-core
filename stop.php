98830dc5e1c8cf327fe27601265a41bb4c3b86f3
Marc <marc@anakeen.com>
2011-07-08 13:12:49 +0200

    maintenance page style (refs #1728)

--- stop.php
+++ stop.php
@@ -1,7 +1,6 @@
 <?php
 /**
- * Display a message to advert that WHAT being to be upgraded
- * replace index.php when wstop is invocated (RPM update)
+ * Display a message to advert that Dynacase Platform being to be upgraded
  *
  * @author Anakeen 2002
  * @version $Id: stop.php,v 1.4 2007/03/06 18:57:03 eric Exp $
@@ -11,16 +10,37 @@
  */
 header("HTTP/1.0 503 Service Unavailable");
 ?>
-<HTML>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
+<html xmlns="http://www.w3.org/1999/xhtml"> 
 <HEAD>
-<TITLE>Maintenance</TITLE>
+<TITLE>Dynacase Maintenance</TITLE>
+<style>
+body { font-family:Trebuchet MS, Helvetica, sans; background-color:#777; padding:0px; margin:0px; color:#444; }
+#hello { padding: 60px 40px; }
+.title { font-size: 400%; font-weight: bold; text-shadow: 0 1px 1px #fff;}
+.content {    font-size: 200%; font-weight: bold; text-shadow: 0 1px 1px #fff; }
+.message { padding-top:40px ; font-size:120%; color : #FF6600; text-shadow: 0 1px 1px #555;  }
+td { vertical-align:top;padding:20px;}
+</style>
 </HEAD>
 
-<BODY  style="">
-<div align="center">
-<div style="width:80%;border: solid 4px red;padding: 0.5em">
-<H2 align="left">The system is currently unavailable due to maintenance works.</H2>
-<H2 align="left">Please come back later.</H2>
-</div></div>
+<body>
+<div id="hello">
+    <table>
+    <tr><td><img src="Images/maintenance-symbol.png" /></td>
+    <td>
+      <div class="title">Dynacase Platform</div>
+      <div class="content">The open source case management platform.</div> 
+      <div class="message">
+         <div>The system is currently unavailable due to maintenance works.</div> 
+         <div>Please come back later.</div>
+      </div>
+      <div class="message">
+         <div>Une op&eacute;ration de maintenance est en cours.</div> 
+         <div>Merci de revenir plus tard.</div>
+      </div>
+    </td></tr>
+    </table>
+</div>
 </BODY>
 </HTML>
