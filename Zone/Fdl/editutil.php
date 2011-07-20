852e6b9a9a49fda9238f5c5d07b5347c11c51007
Éric Brison <eric.brison@anakeen.com>
2011-07-20 17:07:13 +0200

    correct thesaurus edit (refs #1767)

--- Zone/Fdl/editutil.php
+++ Zone/Fdl/editutil.php
@@ -286,7 +286,11 @@ function getHtmlInput(&$doc, &$oattr, $value, $index="",$jsevent="",$notd=false)
 				$lay = new Layout("THESAURUS/Layout/editinputthconcept.xml");
 				if ($value) {
 					$th=new_doc($doc->dbaccess,$value);
+					if ($th->isAlive()) {
 					$thtitle=$th->getLangTitle();
+					} else {
+					    $thtitle='nc';
+					}
 					$lay->set("atitle", $thtitle);
 				} else  $lay->set("atitle", false);
 			}
