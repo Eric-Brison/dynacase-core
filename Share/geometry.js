cbdc31d444b1cfa57543cb80c5ef86917f6a0387
Jérôme Augé <jerome.auge@anakeen.com>
2011-11-30 18:01:08 +0100

    Check that document and document.documentElement are defined (closes #2091)

--- Share/geometry.js
+++ Share/geometry.js
@@ -9,10 +9,12 @@ var Xpos = 0;
 var Ypos = 0;
 function GetXY(event) {
   if (window.event) {
-    Xpos = window.event.clientX + document.documentElement.scrollLeft
-                             + document.body.scrollLeft;
-    Ypos = window.event.clientY + document.documentElement.scrollTop +
-                             + document.body.scrollTop;
+    if( document && document.documentElement ) {
+      Xpos = window.event.clientX + document.documentElement.scrollLeft
+                                  + document.body.scrollLeft;
+      Ypos = window.event.clientY + document.documentElement.scrollTop +
+                                  + document.body.scrollTop;
+    }
   }
   else {
     Xpos = event.clientX + window.scrollX;
