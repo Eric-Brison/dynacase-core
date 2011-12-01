543bf47786d9fff39cdb02219b26211be8805383
Éric Brison <eric.brison@anakeen.com>
2011-12-01 17:50:47 +0100

    add tests for states with error codes (closes #1262)

--- Class/Fdl/Class.CheckWorkflow.php
+++ Class/Fdl/Class.CheckWorkflow.php
@@ -30,32 +30,51 @@ class checkWorkflow
      * @var string
      */
     private $className;
+    
+    const maxTransitionModel = 20;
     /**
      * @var array
      */
-    private $transitionProperties = array(
+    private $transitionModelProperties = array(
         'm1',
         'm2',
         'ask',
         'nr'
     );
+    /**
+     * @var array
+     */
+    private $transitionProperties = array(
+        'e1',
+        'e2',
+        't'
+    );
+    
     public function __construct($className)
     {
         $this->className = $className;
     }
     
-    private function addError($msg)
+    private function addError($code, $msg)
     {
-        if ($msg) $this->terr[] = $msg;
+        if ($msg) {
+            $msg = sprintf("{%s} %s", $code, $msg);
+            if (!in_array($msg, $this->terr)) {
+                $this->terr[] = $msg;
+            }
+        }
     }
+    
     public function getErrorMessage()
     {
         return implode("\n", $this->terr);
     }
+    
     public function getError()
     {
         return $this->terr;
     }
+    
     public function verifyWorflow()
     {
         $this->checkClassName();
@@ -63,6 +82,7 @@ class checkWorkflow
             $this->checkIsAWorkflow();
             if (!$this->getErrorMessage()) {
                 $this->checkPrefix();
+                $this->checkTransitionModels();
                 $this->checkTransitions();
             }
         }
@@ -72,32 +92,63 @@ class checkWorkflow
     public function checkTransitions()
     {
         
+        $cycle = $this->wdoc->cycle;
+        if (!is_array($cycle)) {
+            $this->addError('W0001', sprintf("workflow transition is not an array for class %s", $this->className));
+        } else {
+            foreach ($cycle as $k => $state) {
+                $props = array_keys($state);
+                $diff = array_diff($props, $this->transitionProperties);
+                if (count($diff) > 0) {
+                    $this->addError('W0002', sprintf("workflow transition unknow property %s for transition #%d in class %s (must be one of %s)", implode(',', $diff) , $k, $this->className, implode(',', $this->transitionProperties)));
+                }
+                if ($state["e1"]) $this->checkTransitionStateKey($state["e1"]);
+                if ($state["e2"]) $this->checkTransitionStateKey($state["e2"]);
+                if ($state["t"]) {
+                } else {
+                    $this->addError('W0003', sprintf("workflow transition #%d property 't' is mandatory in class %s", $k, $this->className));
+                }
+            }
+        }
+    }
+    
+    public function checkTransitionModels()
+    {
+        
         $transitions = $this->wdoc->transitions;
         if (!is_array($transitions)) {
-            $this->addError(sprintf("workflow transition is not an array for class %s", $this->className));
+            $this->addError('W0004', sprintf("workflow transition is not an array for class %s", $this->className));
         } else {
+            if (count($transitions) > self::maxTransitionModel) {
+                $this->addError('W0005', sprintf("workflow %s number of transition model (found %d) exceed limit (max is %s)", $this->className, count($transitions) , self::maxTransitionModel));
+            }
             foreach ($transitions as $tkey => $transition) {
                 $this->checkTransitionStateKey($tkey);
                 
                 $props = array_keys($transition);
-                $diff = array_diff($props, $this->transitionProperties);
+                $diff = array_diff($props, $this->transitionModelProperties);
                 if (count($diff) > 0) {
-                    $this->addError(sprintf("workflow transition unknow property %s for transition %s in class %s (must be one of %s)", implode(',', $diff) , $tkey, $this->className, implode(',', $this->transitionProperties)));
+                    $this->addError('W0006', sprintf("workflow transition unknow property %s for transition model %s in class %s (must be one of %s)", implode(',', $diff) , $tkey, $this->className, implode(',', $this->transitionModelProperties)));
                 }
                 
                 if ($transition["ask"] && (!is_array($transition["ask"]))) {
-                    $this->addError(sprintf("workflow transition ask is not an array for transition %s in class %s", $tkey, $this->className));
+                    $this->addError('W0007', sprintf("workflow transition ask is not an array for transition model %s in class %s", $tkey, $this->className));
                 }
                 if ($transition["m1"]) {
                     if (!method_exists($this->wdoc, $transition["m1"])) {
                         
-                        $this->addError(sprintf("workflow unknow m1 method %s for transition %s in class %s", $transition["m1"], $tkey, $this->className));
+                        $this->addError('W0008', sprintf("workflow unknow m1 method %s for transition model %s in class %s", $transition["m1"], $tkey, $this->className));
                     }
                 }
                 if ($transition["m2"]) {
                     if (!method_exists($this->wdoc, $transition["m2"])) {
                         
-                        $this->addError(sprintf("workflow unknow m2 method %s for transition %s in class %s", $transition["m2"], $tkey, $this->className));
+                        $this->addError('W0009', sprintf("workflow unknow m2 method %s for transition model %s in class %s", $transition["m2"], $tkey, $this->className));
+                    }
+                }
+                if (in_array("nr", $props)) {
+                    if (!is_bool($transition["nr"])) {
+                        $this->addError('W0010', sprintf("workflow transition nr property is not a boolean for transition model %s in class %s", implode(',', $diff) , $tkey, $this->className));
                     }
                 }
             }
@@ -108,7 +159,7 @@ class checkWorkflow
     {
         $limit = 45 - strlen($this->wdoc->attrPrefix);
         if (!preg_match("/^[a-zA-Z_][a-zA-Z0-9_:]{0,$limit}$/", $key)) {
-            $this->addError(sprintf("workflow transition or state key %s syntax error for %s (limit to %d alpha characters)", $key, $this->className, $limit + 1));
+            $this->addError('W0011', sprintf("workflow transition or state key %s syntax error for %s (limit to %d alpha characters)", $key, $this->className, $limit + 1));
         }
     }
     
@@ -124,11 +175,11 @@ class checkWorkflow
         // Get the shell output from the syntax check command
         exec(sprintf('php -l %s 2>/dev/null', escapeshellarg($fileName)) , $output, $status);
         if ($status != 0) {
-            $this->addError(implode("\n", $output));
+            $this->addError('W0012', implode("\n", $output));
         } else {
             include_once ($this->getWorkflowClassFile());
             if (!class_exists($this->className)) {
-                $this->addError(sprintf("workflow class %s not found", $this->className));
+                $this->addError('W0013', sprintf("workflow class %s not found", $this->className));
             } else {
                 $class = $this->className;
                 $this->wdoc = new $class();
@@ -141,24 +192,24 @@ class checkWorkflow
     public function errorHandler($errno, $errstr, $errfile, $errline)
     {
         
-        $this->addError("handler:" . $errstr);
+        $this->addError('W0000', "handler:" . $errstr);
     }
     
     public function checkPrefix()
     {
         if (!$this->wdoc->attrPrefix) {
-            $this->addError(sprintf("workflow : missing attrPrefix definition for %s class", $this->className));
+            $this->addError('W0014', sprintf("workflow : missing attrPrefix definition for %s class", $this->className));
         } elseif (!preg_match('/^[a-zA-Z_][a-zA-Z0-9_]{0,9}$/', $this->wdoc->attrPrefix)) {
-            $this->addError(sprintf("workflow : syntax error attrPrefix for %s class (limit to 10 alpha characters)", $this->className));
+            $this->addError('W0015', sprintf("workflow : syntax error attrPrefix for %s class (limit to 10 alpha characters)", $this->className));
         }
     }
     
     public function checkClassName()
     {
         if (empty($this->className)) {
-            $this->addError(sprintf("workflow class name is empty"));
+            $this->addError('W0016', sprintf("workflow class name is empty"));
         } elseif (!self::checkPhpClass($this->className)) {
-            $this->addError(sprintf("class name %s not valid", $this->className));
+            $this->addError('W0017', sprintf("class name %s not valid", $this->className));
         } else {
             $this->checkFileName();
         }
@@ -167,9 +218,10 @@ class checkWorkflow
     public function checkFileName()
     {
         if (!file_exists($this->getWorkflowClassFile())) {
-            $this->addError(sprintf("file name for %s not found", $this->className));
+            $this->addError('W0018', sprintf("file name for %s not found", $this->className));
         }
     }
+    
     public static function checkPhpClass($name)
     {
         if (preg_match('/^[a-zA-Z_][a-zA-Z0-9_]+$/', $name)) {
--- Test/control/PU_test_dcp_importfamily.php
+++ Test/control/PU_test_dcp_importfamily.php
@@ -97,78 +97,114 @@ class TestImportFamily extends TestCaseDcpDocument
             )
         );
     }
+    
     public function dataBadFamilyFiles()
     {
         return array(
             // test attribute too long
             array(
                 "PU_data_dcp_badfamily1.ods",
-                "AAAAAA"
+                array(
+                    "FA002",
+                    "AAAAAA"
+                )
             ) ,
             // test method not found
             array(
                 "PU_data_dcp_badfamily2.ods",
-                "Method.NotFound"
+                array(
+                    "Method.NotFound"
+                )
             ) ,
             // test order needed
             array(
                 "PU_data_dcp_badfamily3.ods",
-                "TST_ARRAY"
-            ) ,
-            // test order needed
-            array(
-                "PU_data_dcp_badfamily3.ods",
-                "TST_TITLE"
-            ) ,
-            // test visibility
-            array(
-                "PU_data_dcp_badfamily3.ods",
-                "W3"
+                array(
+                    "FA003",
+                    "FA006",
+                    "FA004",
+                    "TST_ARRAY",
+                    "TST_TITLE",
+                    "W3"
+                )
             ) ,
             // test workflow
             array(
                 "PU_data_dcp_badfamily4.ods",
-                "WTST_WFFAMIMP4"
+                array(
+                    "W0008",
+                    "WTST_WFFAMIMP4"
+                )
             ) ,
             // test workflow class name syntax error
             array(
                 "PU_data_dcp_badfamily5.ods",
-                "WImpTest5"
+                array(
+                    "W0017",
+                    "WImpTest5"
+                )
             ) ,
             // test workflow without class
             array(
                 "PU_data_dcp_badfamily6.ods",
-                "WNotFound"
+                array(
+                    "W0018",
+                    "WNotFound"
+                )
             ) ,
             // test workflow with a incorrect class name
             array(
                 "PU_data_dcp_badfamily7.ods",
-                "WTestBadNameImp7"
+                array(
+                    "W0013",
+                    "WTestBadNameImp7"
+                )
             ) ,
             // test workflow with class syntax error
             array(
                 "PU_data_dcp_badfamily8.ods",
-                "Parse error"
+                array(
+                    "W0012",
+                    "Parse error"
+                )
             ) ,
             // test workflow attrPrefix empty
             array(
                 "PU_data_dcp_badfamily9.ods",
-                "attrPrefix"
+                array(
+                    "W0008",
+                    "W0014",
+                    "attrPrefix"
+                )
             ) ,
             // test workflow attrPrefix syntax
             array(
                 "PU_data_dcp_badfamily10.ods",
-                "syntax"
+                array(
+                    "W0015",
+                    "syntax"
+                ) ,
             ) ,
-            // test workflow transition definition
+            // test workflow transition model definition
             array(
                 "PU_data_dcp_badfamily11.ods",
                 array(
-                    " m1 ",
-                    " m2 ",
-                    "T sick",
-                    "m5",
-                    "ask"
+                    "W0006",
+                    "W0007",
+                    "W0008",
+                    "W0009",
+                    "W0011"
+                )
+            ) ,
+            // test workflow transition definition
+            array(
+                "PU_data_dcp_badfamily12.ods",
+                array(
+                    "W0011",
+                    "W0002",
+                    "W0003",
+                    "dead or not",
+                    " e3 "
                 )
             )
         );
