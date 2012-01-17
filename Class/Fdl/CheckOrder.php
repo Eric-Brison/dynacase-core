e709276e9c8323e83dc621e3d5a0e1345593344c
Éric Brison <eric.brison@anakeen.com>
2012-01-17 19:08:10 +0100

    correct tests (refs #2282)

--- Class/Fdl/CheckOrder.php
+++ Class/Fdl/CheckOrder.php
@@ -76,7 +76,7 @@ class CheckOrder extends CheckData
     {
         if ($this->family) {
             foreach ($this->attrIds as $aid) {
-                if (!$this->family->getAttribute($aid)) {
+                if ($aid && (!$this->family->getAttribute($aid))) {
                     $this->addError(ErrorCode::getError('ORDR0100', $aid, $this->family->name));
                 }
             }
