f4bcba0537a977dbc6448d7c1769ee5bf22a642e
Éric Brison <eric.brison@anakeen.com>
2012-02-10 14:21:57 +0100

    update comments in core css

--- Action/Core/core_css.php
+++ Action/Core/core_css.php
@@ -5,25 +5,19 @@
  * @package FDL
 */
 /**
- * Generated Header (not documented yet)
+ * send css file
  *
  * @author Anakeen 2000
- * @version $Id: core_css.php,v 1.6 2005/09/27 15:01:15 eric Exp $
- * @license http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License
  * @package FDL
  * @subpackage CORE
  */
 /**
  */
-// ---------------------------------------------------------------
-// $Id: core_css.php,v 1.6 2005/09/27 15:01:15 eric Exp $
-// $Source: /home/cvsroot/anakeen/freedom/core/Action/Core/core_css.php,v $
-// ---------------------------------------------------------------
-function core_css(&$action)
+function core_css(Action & $action)
 {
     
-    $layout = getHttpVars("layout");
-    $type = getHttpVars("type");
+    $layout = $action->getArgument("layout");
+    $type = $action->getArgument("type");
     
     if (preg_match("/([A-Z_-]+):([^:]+):{0,1}[A-Z]{0,1}/", $layout, $reg)) {
         $lfile = getLayoutFile($reg[1], strtolower($reg[2]));
