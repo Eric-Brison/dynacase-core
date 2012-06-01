1d76e76efc6923b65772106a5f4fb9ac2dc1077e
Nicolas <nicolas.thing-leoh@anakeen.com>
2012-06-01 17:03:04 +0200

    Refactoring documentation: Hisotrique in another file (refs #2582) Beautify of code (refs #2584)

--- Action/Freedom/setlogicalname.php
+++ Action/Freedom/setlogicalname.php
@@ -18,29 +18,28 @@
 
 include_once ("FDL/Class.Doc.php");
 
-function setlogicalname(Action & $action) {
-
+function setlogicalname(Action & $action)
+{
     $dbaccess = $action->GetParam("FREEDOM_DB");
     $docid = GetHttpVars("id");
     $name = GetHttpVars("name");
     if ($docid && $name) {
         $doc = new_Doc($dbaccess, $docid, true);
         if (!$doc->isAlive()) {
-            $action->addWarningMsg(sprintf(_("Document %s is not alive"), $doc->getTitle()));
+            $action->addWarningMsg(sprintf(_("Document %s is not alive") , $doc->getTitle()));
         } else {
             $oldName = $doc->name;
             $err = $doc->setLogicalIdentificator($name, true);
             if ($err != "") $action->addWarningMsg($err);
             else {
                 if ($oldName) {
-                    $doc->addComment(sprintf(_("update logical name from %s to %s"), $oldName, $doc->name));
+                    $doc->addComment(sprintf(_("update logical name from %s to %s") , $oldName, $doc->name));
                 } else {
-                    $doc->addComment(sprintf(_("set logical name to %s"), $doc->name));
+                    $doc->addComment(sprintf(_("set logical name to %s") , $doc->name));
                 }
             }
         }
     }
     redirect($action, "FDL", "IMPCARD&zone=FDL:VIEWPROPERTIES:T&id=" . $docid);
 }
-
 ?>
