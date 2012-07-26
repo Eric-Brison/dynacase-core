e0437c4d8e242ef787885cabbb573e0ef2015919
Jérôme Augé <jerome.auge@anakeen.com>
2012-07-26 11:47:49 +0200

    Report errors when deleting app with "appadmin" API script (closes #2843)

--- Api/appadmin.php
+++ Api/appadmin.php
@@ -19,6 +19,7 @@
 /**
  */
 include_once ("Class.Application.php");
+global $action;
 
 $usage = new ApiUsage();
 $usage->setText("Manage application");
@@ -39,15 +40,27 @@ $app = new Application();
 $Null = "";
 if ($method != "delete") {
     $app->Set($appname, $Null, null, true);
-    if ($method == "reinit") $app->InitApp($appname, false, null, true);
-    if ($method == "update") $app->InitApp($appname, true);
+    if ($method == "reinit") {
+        $ret = $app->InitApp($appname, false, null, true);
+        if ($ret === false) {
+            $action->exitError(sprintf("Error initializing application '%s'.", $appname));
+        }
+    }
+    if ($method == "update") {
+        $ret = $app->InitApp($appname, true);
+        if ($ret === false) {
+            $action->exitError(sprintf("Error updating application '%s'.", $appname));
+        }
+    }
 }
 if ($method == "delete") {
     $app->Set($appname, $Null, null, false);
     if ($app->isAffected()) {
-        $app->DeleteApp();
+        $err = $app->DeleteApp();
+        if ($err != '') {
+            $action->exitError($err);
+        }
     } else {
         echo "already deleted";
     }
 }
-?>
