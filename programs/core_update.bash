4f51a03a49713cc0d13cd1ed65c2cb3bde29a0ea
Jérôme Augé <jerome.auge@anakeen.com>
2012-08-21 12:18:50 +0200

    Removed DYNACASECONTROL_* parameters (closes #2662)
    The parameters are removed because the mechanism to get wiff-path and
    wiff-url does not exists yet.

--- programs/core_update.bash
+++ programs/core_update.bash
@@ -62,11 +62,3 @@ fi
 
 export wpub=$WIFF_CONTEXT_ROOT # same as `wiff --getValue=rootdirectory`
 . "$WIFF_CONTEXT_ROOT"/programs/core_environment
-
-# set dynacase control acces url & path 
-dc_path=`"$WIFF_ROOT"/wiff config get wiff-path`
-dc_url=`"$WIFF_ROOT"/wiff config get wiff-url`
-
-"$wpub"/wsh.php --api=set_param --param="DYNACASECONTROL_PATH" --value="$dc_path"
-"$wpub"/wsh.php --api=set_param --param="DYNACASECONTROL_URL" --value="$dc_url"
-
