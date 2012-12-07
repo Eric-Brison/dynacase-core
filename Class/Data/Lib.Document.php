6c96b66915a0785adcaace90a319e4414589abc0
Charles Bonnissent <charles.bonnissent@anakeen.com>
2012-12-07 11:35:36 +0100

    Export method getSearchCriteria (closes #3358)

--- Class/Data/Lib.Document.php
+++ Class/Data/Lib.Document.php
@@ -43,4 +43,52 @@ function searchByTitle($famid, $key)
     }
     return $out;
 }
-?>
+
+function getSearchCriteria(stdClass &$out)
+{
+    $s = new DocSearch();
+    $operators = $s->top;
+    $out->error = '';
+    $out->operators = array();
+    $alltype = array(
+        'integer',
+        'text',
+        'longtext',
+        'htmltext',
+        'docid',
+        'double',
+        "ifile",
+        "array",
+        "file",
+        "image",
+        "enum",
+        "color",
+        "date",
+        "time",
+        "timestamp",
+        "money"
+    );
+    foreach ($operators as $k => $v) {
+        if (empty($v["type"]) || (!is_array($v["type"]))) $v["type"] = $alltype;
+        foreach ($v["type"] as $type) {
+            $label = (!empty($v["label"])) ? _($v["label"]) : '';
+            $dynlabel = (!empty($v["dynlabel"])) ? _($v["dynlabel"]) : '';
+            if (!empty($v["slabel"])) {
+                foreach ($v["slabel"] as $kl => $vl) {
+                    if ($kl == $type) $label = _($vl);
+                }
+            }
+            if (!empty($v["sdynlabel"])) {
+                foreach ($v["sdynlabel"] as $kl => $vl) {
+                    if ($kl == $type) $dynlabel = _($vl);
+                }
+            }
+            $out->operators[$type][] = array(
+                "operator" => $k,
+                "operand" => $v["operand"],
+                "label" => $label,
+                "tplLabel" => $dynlabel
+            );
+        }
+    }
+}
