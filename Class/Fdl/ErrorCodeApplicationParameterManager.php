cedad45fbf2c720b27114847468a2befb79cd9f9
Charles Bonnissent <charles.bonnissent@anakeen.com>
2013-01-16 12:12:01 +0100

    Add a new application parameter manager (closes #3478)
    Add a new class to handle parameter management
    Add TAUFU associated

---
+++ Class/Fdl/ErrorCodeApplicationParameterManager.php
@@ -0,0 +1,82 @@
+<?php
+/*
+ * @author Anakeen
+ * @license http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License
+ * @package FDL
+*/
+namespace {
+    /**
+     * ErrorCodeApplicationParameterManager
+     *
+     * @see ApplicationParameterManager
+     */
+    class ErrorCodeAPM
+    {
+        /**
+         * @errorCode The application name not exists
+         */
+        const APM0001 = 'The application id "%s" is not found';
+        /**
+         * @errorCode The global parameter key not exists
+         */
+        const APM0002 = 'Global parameter "%s" is not found';
+        /**
+         * @errorCode The application name not exists
+         */
+        const APM0003 = 'The application name "%s" is not found';
+        /**
+         * @errorCode The type of the application element is not usable
+         */
+        const APM0004 = 'The application type is not usable';
+        /**
+         * @errorCode The type of the user element is not usable
+         */
+        const APM0005 = 'The user type is not usable';
+        /**
+         * @errorCode Error during record user application parameter value
+         */
+        const APM0006 = 'Cannot set user parameter "%s" for application "%s"';
+        /**
+         * @errorCode The parameter key not exists
+         * also when parameter is not declared as user and global
+         */
+        const APM0007 = 'Cannot set User parameter "%s" "%s" user "#%s" not found';
+        /**
+         * @errorCode Unable to get parameter
+         */
+        const APM008 = 'The parameter %s of the application %s is not found ';
+        /**
+         * @errorCode Error during record  application parameter value
+         */
+        const APM0009 = 'Cannot set parameter "%s" for application "%s" : %s';
+        /**
+         * @errorCode Unable to get application id, when parameter is global need a parameter name
+         */
+        const APM0010 = 'Unable to get application id, when parameter is global need a parameter name';
+
+        /**
+         * for beautifier
+         */
+        private function _bo()
+        {
+            if (true) $a = 1;
+        }
+    }
+}
+
+namespace Dcp\ApplicationParameterManager {
+    /**
+     * @errorCode
+     * Account search exception
+     */
+    class Exception extends \Dcp\Exception
+    {
+        /**
+         * for beautifier
+         */
+        private function _bo()
+        {
+            if (true) $a = 1;
+        }
+    }
+}
---
+++ Test/control/PU_test_dcp_applicationParameterManager.php
@@ -0,0 +1,412 @@
+<?php
+/*
+ * @author Anakeen
+ * @license http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License
+ * @package FDL
+*/
+
+namespace Dcp\Pu;
+/**
+ * @author Anakeen
+ * @license http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License
+ * @package Dcp\Pu
+ */
+
+require_once 'PU_testcase_dcp.php';
+
+class TestApplicationParameterManeger extends TestCaseDcp
+{
+
+    const appName = "DCPTEST2";
+
+    /**
+     * @return \Application
+     */
+    private function initTestApplication($parameters)
+    {
+        $appTest = new \Application(self::$dbaccess);
+        $appTest->name = self::appName;
+        $appTest->childof = 'ONEFAM';
+        $err = $appTest->Add();
+
+        $this->assertEmpty($err, "Cannot create application : $err");
+        $parent = null;
+        $appTest->set(self::appName, $parent);
+
+        $this->assertTrue($appTest->isAffected(), sprintf("DCPTEST2 app not found"));
+
+        $appTest->InitAllParam($parameters, $update = false);
+        $a = $this->getAction();
+        // add new parameters in current action
+        $a->parent->param->SetKey($appTest->id, $a->user->id);
+
+        \ApplicationParameterManager::resetCache();
+        return $appTest;
+    }
+
+    /**
+     * @dataProvider dataGetParam
+     */
+    public function testGetParam($parameters, array $expectedValues)
+    {
+
+        $this->initTestApplication($parameters);
+
+        foreach ($expectedValues as $k => $v) {
+            $this->assertEquals($v, \ApplicationParameterManager::getParameterValue(self::appName, $k), sprintf("wrong value for %s", $k));
+        }
+    }
+
+    /**
+     * @dataProvider dataGetParam
+     * @expectedException Dcp\ApplicationParameterManager\Exception
+     */
+    public function testGetUnknownParam($parameters)
+    {
+
+        $this->initTestApplication($parameters);
+
+        \ApplicationParameterManager::getParameterValue(self::appName, "UNKNOWN_OPTION_VALUE");
+    }
+
+    /**
+     * @dataProvider dataGetParam
+     * @expectedException Dcp\ApplicationParameterManager\Exception
+     */
+    public function testSetUnknownParam($parameters)
+    {
+
+        $this->initTestApplication($parameters);
+
+        \ApplicationParameterManager::setParameterValue(self::appName, "UNKNOWN_OPTION_VALUE", "25");
+    }
+
+    /**
+     * @dataProvider dataGetParam
+     */
+    public function testGetScopedParam($parameters, array $expectedValues)
+    {
+
+        $this->initTestApplication($parameters);
+
+        foreach ($expectedValues as $k => $v) {
+            $this->assertEquals($v, \ApplicationParameterManager::getScopedParameterValue($k), sprintf("wrong value for %s", $k));
+        }
+    }
+
+    /**
+     * @dataProvider dataGetGlobalParam
+     */
+    public function testGetGlobalParam($parameters, array $expectedValues)
+    {
+
+        $this->initTestApplication($parameters);
+
+        foreach ($expectedValues as $k => $v) {
+            $this->assertEquals($v, \ApplicationParameterManager::getCommonParameterValue(\ApplicationParameterManager::GLOBAL_PARAMETER, $k), sprintf("wrong value for %s", $k));
+        }
+    }
+
+    /**
+     * @dataProvider dataSetApplicationParameter
+     */
+    public function testSetApplicationParameter($parameters, array $newValues, array $expectedValues)
+    {
+
+        $this->initTestApplication($parameters);
+
+        foreach ($newValues as $k => $v) {
+            \ApplicationParameterManager::setParameterValue(self::appName, $k, $v);
+        }
+        foreach ($expectedValues as $k => $v) {
+            $this->assertEquals($v, \ApplicationParameterManager::getParameterValue(self::appName, $k), sprintf("wrong value for %s", $k));
+        }
+    }
+
+    /**
+     * @dataProvider dataSetGlobalParameter
+     */
+    public function testSetGlobalParameter($parameters, array $newValues, array $expectedValues)
+    {
+
+        $this->initTestApplication($parameters);
+
+        foreach ($newValues as $k => $v) {
+
+            \ApplicationParameterManager::setParameterValue(\ApplicationParameterManager::GLOBAL_PARAMETER,$k, $v);
+        }
+        foreach ($expectedValues as $k => $v) {
+            $this->assertEquals($v, \ApplicationParameterManager::getParameterValue(\ApplicationParameterManager::GLOBAL_PARAMETER,$k), sprintf("wrong value for %s", $k));
+        }
+    }
+
+    /**
+     * @dataProvider dataSetUserApplicationParameter
+     */
+    public function testSetUserApplicationParameter($parameters, array $newValues, array $expectedValues)
+    {
+
+        $this->initTestApplication($parameters);
+
+        foreach ($newValues as $k => $v) {
+            \ApplicationParameterManager::setParameterValue(self::appName, $k, $v);
+        }
+        foreach ($expectedValues as $k => $v) {
+            $this->assertEquals($v, \ApplicationParameterManager::getParameterValue(self::appName, $k), sprintf("wrong value for %s", $k));
+        }
+    }
+
+    /**
+     * @dataProvider dataGetParameters
+     */
+    public function testGetParameters($parameters)
+    {
+
+        $definedParametersKey = array_keys($parameters);
+
+        $this->initTestApplication($parameters);
+        $applicationParameters = \ApplicationParameterManager::getParameters(self::appName);
+        $filterFunction = function($value) use ($applicationParameters) {
+            foreach ($applicationParameters as $currentApplicationParameters) {
+                if ($currentApplicationParameters["name"] == $value) {
+                    return false;
+                }
+            }
+            return true;
+        };
+        $definedParametersKey = array_filter($definedParametersKey, $filterFunction);
+        $this->assertEquals(count($definedParametersKey), 0);
+    }
+
+    /**
+     * @dataProvider dataGetParameters
+     */
+    public function testGetParameter($parameters)
+    {
+        $this->initTestApplication($parameters);
+        $applicationParameter = \ApplicationParameterManager::getParameter(self::appName, "TST_GLOB1");
+
+        $this->assertEquals("TST_GLOB1", $applicationParameter["name"]);
+        $this->assertEquals("Name of the glob", $applicationParameter["descr"]);
+        $this->assertEquals("Y", $applicationParameter["isglob"]);
+        $this->assertEquals("N", $applicationParameter["isuser"]);
+    }
+
+
+
+    public function dataUserSetGlobalParameter()
+    {
+        return array(
+            array(
+                "init" => array(
+                    "VERSION" => "4.0.8",
+                    "ONEFAM_MIDS" => "128",
+                    "TST_NAMEP1" => array(
+                        "val" => "Test 2",
+                        "descr" => "Name of test one",
+                        "global" => "Y",
+                        "user" => "Y"
+                    ),
+                    "TST_GLOB2" => array(
+                        "val" => "Test global 2",
+                        "descr" => "Name of the glob",
+                        "global" => "Y",
+                        "user" => "Y"
+                    )
+                ),
+                "set" => array(
+                    "TST_NAMEP1" => "Test 3",
+                    "TST_GLOB2" => "Test G3",
+                ),
+                "expect" => array(
+                    "TST_NAMEP1" => "Test 3",
+                    "VERSION" => "4.0.8",
+                    "TST_GLOB2" => "Test G3"
+                )
+            )
+        );
+    }
+
+    public function dataSetUserApplicationParameter()
+    {
+        return array(
+            array(
+                "init" => array(
+                    "VERSION" => "0.3.2-2",
+                    "ONEFAM_MIDS" => "128",
+                    "TST_NAMEP1" => array(
+                        "val" => "Test 2",
+                        "descr" => "Name of test one",
+                        "global" => "N",
+                        "user" => "Y"
+                    ),
+                    "TST_GLOB2" => array(
+                        "val" => "Test global 2",
+                        "descr" => "Name of the glob",
+                        "global" => "Y",
+                        "user" => "Y"
+                    )
+                ),
+                "set" => array(
+                    "ONEFAM_IDS" => "128,127",
+                    "TST_NAMEP1" => "Test 3",
+                ),
+                "expect" => array(
+                    "TST_NAMEP1" => "Test 3",
+                    "ONEFAM_IDS" => "128,127",
+                    "TST_GLOB2" => "Test global 2"
+                )
+            )
+        );
+    }
+
+    public function dataSetGlobalParameter()
+    {
+        return array(
+            array(
+                "init" => array(
+                    "VERSION" => "4.0.8",
+                    "ONEFAM_MIDS" => "128",
+                    "TST_NAMEP1" => array(
+                        "val" => "Test 2",
+                        "descr" => "Name of test one",
+                        "global" => "Y",
+                        "user" => "N"
+                    ),
+                    "TST_GLOB2" => array(
+                        "val" => "Test global 2",
+                        "descr" => "Name of the glob",
+                        "global" => "Y",
+                        "user" => "N"
+                    )
+                ),
+                "set" => array(
+                    "TST_NAMEP1" => "Test 3",
+                    "TST_GLOB2" => "Test G3",
+                ),
+                "expect" => array(
+                    "TST_NAMEP1" => "Test 3",
+                    "TST_GLOB2" => "Test G3"
+                )
+            )
+        );
+    }
+
+    public function dataSetApplicationParameter()
+    {
+        return array(
+            array(
+                "init" => array(
+                    "VERSION" => "0.3.2-2",
+                    "ONEFAM_MIDS" => "128",
+                    "TST_NAMEP1" => array(
+                        "val" => "Test 2",
+                        "descr" => "Name of test one",
+                        "global" => "N",
+                        "user" => "N"
+                    ),
+                    "TST_GLOB2" => array(
+                        "val" => "Test global 2",
+                        "descr" => "Name of the glob",
+                        "global" => "Y",
+                        "user" => "N"
+                    )
+                ),
+                "set" => array(
+                    "VERSION" => "4.0.7",
+                    "ONEFAM_MIDS" => "128,127",
+                    "TST_NAMEP1" => "Test 3",
+                ),
+                "expect" => array(
+                    "TST_NAMEP1" => "Test 3",
+                    "VERSION" => "4.0.7",
+                    "ONEFAM_MIDS" => "128,127",
+                    "TST_GLOB2" => "Test global 2"
+                )
+            )
+        );
+    }
+
+    public function dataGetParam()
+    {
+        return array(
+            array(
+                array(
+                    "VERSION" => "0.3.2-2",
+                    "ONEFAM_MIDS" => "128",
+                    "TST_NAMEP1" => array(
+                        "val" => "Test 1",
+                        "descr" => "Name of test one",
+                        "global" => "N",
+                        "user" => "N"
+                    ),
+                    "TST_GLOB1" => array(
+                        "val" => "Test global 1",
+                        "descr" => "Name of the glob",
+                        "global" => "Y",
+                        "user" => "N"
+                    )
+                ),
+                array(
+                    "TST_NAMEP1" => "Test 1",
+                    "VERSION" => "0.3.2-2",
+                    "ONEFAM_MIDS" => "128",
+                    "TST_GLOB1" => "Test global 1"
+                )
+            )
+        );
+    }
+
+    public function dataGetGlobalParam()
+    {
+        return array(
+            array(
+                array(
+                    "VERSION" => "0.3.2-2",
+                    "ONEFAM_MIDS" => "128",
+                    "TST_NAMEP1" => array(
+                        "val" => "Test 1",
+                        "descr" => "Name of test one",
+                        "global" => "N",
+                        "user" => "N"
+                    ),
+                    "TST_GLOB1" => array(
+                        "val" => "Test global 2",
+                        "descr" => "Name of the glob",
+                        "global" => "Y",
+                        "user" => "N"
+                    )
+                ),
+                array(
+                    "TST_GLOB1" => "Test global 2"
+                )
+            )
+        );
+    }
+
+    public function dataGetParameters()
+        {
+            return array(
+                array(
+                    array(
+                        "VERSION" => "0.3.2-2",
+                        "ONEFAM_MIDS" => "128",
+                        "TST_NAMEP1" => array(
+                            "val" => "Test 1",
+                            "descr" => "Name of test one",
+                            "global" => "N",
+                            "user" => "N"
+                        ),
+                        "TST_GLOB1" => array(
+                            "val" => "Test global 2",
+                            "descr" => "Name of the glob",
+                            "global" => "Y",
+                            "user" => "N"
+                        )
+                    )
+                )
+            );
+        }
+}
+
+?>
