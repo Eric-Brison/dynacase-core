fd6c0e5f13c0cd230b1ba108d1c2b6a64638e638
Éric Brison <eric.brison@anakeen.com>
2013-02-08 10:35:28 +0100

    notice (refs #3410)

--- Class/Fdl/Method.BatchDoc.php
+++ Class/Fdl/Method.BatchDoc.php
@@ -15,39 +15,37 @@
  */
 /**
  */
-
-        /**
-         * @begin-method-ignore
-         * this part will be deleted when construct document class until end-method-ignore
-         */
-        class _BATCH extends _PORTFOLIO
-        {
-            /*
-             * @end-method-ignore
-            */
-function filterContent()
-{
-}
 /**
- * return document includes in portfolio an in each of its guide or searched inside portfolio
- * @param bool $controlview if false all document are returned else only visible for current user  document are return
- * @param array $filter to add list sql filter for selected document
- * @param int $famid family identifier to restrict search
- * @param bool $insertguide if true merge each content of guide else same as a normal folder
- * @return array array of document array
+ * @begin-method-ignore
+ * this part will be deleted when construct document class until end-method-ignore
  */
-function getContent($controlview = true, $filter = array() , $famid = "", $insertguide = true)
+class _BATCH extends _PORTFOLIO
 {
-    
-    return parent::getContent($controlview, $filter, $famid, $insertguide);
+    /*
+     * @end-method-ignore
+    */
+    function filterContent()
+    {
+    }
+    /**
+     * return document includes in portfolio an in each of its guide or searched inside portfolio
+     * @param bool $controlview if false all document are returned else only visible for current user  document are return
+     * @param array $filter to add list sql filter for selected document
+     * @param int $famid family identifier to restrict search
+     * @param bool $insertguide if true merge each content of guide else same as a normal folder
+     * @return array array of document array
+     */
+    function getContent($controlview = true, array $filter = array() , $famid = "", $insertguide = true, $unused = "")
+    {
+        
+        return parent::getContent($controlview, $filter, $famid, $insertguide);
+    }
+    /**
+     * @begin-method-ignore
+     * this part will be deleted when construct document class until end-method-ignore
+     */
 }
-            /**
-             * @begin-method-ignore
-             * this part will be deleted when construct document class until end-method-ignore
-             */
-        }
-        /*
-         * @end-method-ignore
-        */
-
+/*
+ * @end-method-ignore
+*/
 ?>
