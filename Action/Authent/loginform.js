ecf0057f88c906c4580ad096beba70ed4a801723
Nicolas <nicolas.thing-leoh@anakeen.com>
2013-03-05 16:35:02 +0100

    Same css for all AUTHENT action (refs #3689)

---
+++ Action/Authent/loginform.js
@@ -0,0 +1,81 @@
+var isNN = (navigator.appName.indexOf("Netscape") != -1);
+if (isNN) {
+    document.captureEvents(Event.KEYPRESS);
+}
+document.onkeypress = trackEnter;
+
+function trackEnter(evt) {
+    var intKeyCode;
+
+    if (isNN) {
+        intKeyCode = evt.which;
+    }
+    else {
+        intKeyCode = window.event.keyCode;
+    }
+
+    if (intKeyCode == 13) { // enter key
+        login(document.loginform, "[TEXT:warning_name]", '[TEXT:warning_pass]');
+        return false;
+    } else
+        return true;
+}
+
+function login(aform, usermsg, passmsg) {
+    var user = aform.auth_user.value;
+    var pass = aform.auth_pass.value;
+    if (user == "") {
+        document.getElementById('msgerr').innerHTML = usermsg;
+        document.forms['loginform'].elements['auth_user'].focus();
+        return;
+    }
+    if (pass == "") {
+        document.getElementById('msgerr').innerHTML = passmsg;
+        document.forms['loginform'].elements['auth_pass'].focus();
+        return;
+    }
+    aform.submit();
+}
+
+function aumilieu(eid) {
+    var winH = getFrameHeight();
+    var winW = getFrameWidth();
+    if (document.getElementById) { // DOM3 = IE5, NS6
+        var divlogo = document.getElementById(eid);
+        if (divlogo) {
+            divlogo.style.position = 'absolute';
+            if ((winH > 0) && (winW > 0)) {
+                divlogo.style.top = (winH / 2 - (divlogo.offsetHeight / 2) + document.body.scrollTop) + 'px';
+                divlogo.style.left = (winW / 2 - (divlogo.offsetWidth / 2)) + 'px';
+            }
+        }
+    }
+    return true;
+}
+
+function display_help() {
+    aumilieu('zonehelp', 2);
+    document.getElementById('zonehelp').style.visibility = 'visible';
+    document.getElementById('zonehelp').style.zIndex = 100;
+}
+function close_help() {
+    document.getElementById('zonehelp').style.visibility = 'hidden';
+}
+function centerZone() {
+    aumilieu('main');
+    aumilieu('zonehelp');
+}
+function initZone() {
+    centerZone();
+    var focus = document.getElementById("passfocus");
+    if (focus) {
+     focus = document.getElementById("passfocus").value ? 'auth_pass' : 'auth_user';
+        var ifocus = document.getElementById(focus);
+        if (ifocus) ifocus.focus();
+    }
+    if (document.getElementById('zonehelp')) document.getElementById('zonehelp').style.visibility = 'hidden';
+    document.getElementById('main').style.visibility = 'visible';
+
+}
+addEvent(window, 'load', initZone);
+addEvent(window, 'resize', centerZone);
