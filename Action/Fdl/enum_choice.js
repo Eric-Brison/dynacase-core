23bc0a02e6a57d7ddb01bad60e505d6a96929f23
Matthieu Codron <matthieu.codron+dev@anakeen.com>
2013-03-15 19:22:33 +0100

    enum_choice.js beautification (refs #3799)

--- Action/Fdl/enum_choice.js
+++ Action/Fdl/enum_choice.js
@@ -1,10 +1,9 @@
-
 /**
  * @author Anakeen
  * @license http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License
  */
 
-var notalone=true;
+var notalone = true;
 function completechoice(index, tattrid, tattrv, winfo) {
     var rvalue, i, attrid;
     for (i = 0; i < tattrid.length; i++) {
@@ -54,52 +53,45 @@ function completechoice(index, tattrid, tattrv, winfo) {
     return;
 }
 
-var isNetscape = navigator.appName=="Netscape";
-
-
-
+var isNetscape = navigator.appName == "Netscape";
 
 
 function completechoices() {
-    var cvalues = new Array();
-    var i=0;
-    var c=0;
-    for (i=0; i< tattrid.length; i++) {
-      cvalues[i] ="";
+    var cvalues = [];
+    var i = 0;
+    var c = 0;
+    for (i = 0; i < tattrid.length; i++) {
+        cvalues[i] = "";
     }
     senum = document.getElementById('schoose');
-    for (c=0; c< senum.length; c++) {
-      if (senum.options[c].selected) {
-	index= senum.options[c].value;
-	for (i=0; i< tattrid.length; i++) {
-	  with (winfo.document.getElementById(tattrid[i])) {
-	    if (tattrv[index][i] != "") {
-	      cvalues[i] += tattrv[index][i];
-	       cvalues[i] += "\n";
-	      style.backgroundColor='[COLOR_C8]';
-	    }
-	    //       style.fontWeight='bold';
-	  }
-	}
-      }
+    for (c = 0; c < senum.length; c++) {
+        if (senum.options[c].selected) {
+            index = senum.options[c].value;
+            for (i = 0; i < tattrid.length; i++) {
+                var currentAttr = winfo.document.getElementById(tattrid[i]);
+                if (tattrv[index][i] != "") {
+                    cvalues[i] += tattrv[index][i];
+                    cvalues[i] += "\n";
+                    currentAttr.style.backgroundColor = '[COLOR_C8]';
+                }
+            }
+        }
     }
-    for (i=0; i< tattrid.length; i++) {
-      if (cvalues[i][0] != '?')
-	// delete last CR
-	winfo.document.getElementById(tattrid[i]).value = cvalues[i].substring(0,cvalues[i].length-1);
+    for (i = 0; i < tattrid.length; i++) {
+        if (cvalues[i][0] != '?'){
+            // delete last CR
+            winfo.document.getElementById(tattrid[i]).value = cvalues[i].substring(0, cvalues[i].length - 1);
+        }
     }
     winfo.disableReadAttribute();
 }
 
 function autoClose() {
-  // see if only one possibility
-  if (tattrv.length == 1) {
-     completechoice(0,tattrid,tattrv,winfo);
-     setTimeout('self.close()',200); // must be set in next event loop cause Mozilla crash sometimes
-  }
-
-
-
+    // see if only one possibility
+    if (tattrv.length == 1) {
+        completechoice(0, tattrid, tattrv, winfo);
+        setTimeout('self.close()', 200); // must be set in next event loop cause Mozilla crash sometimes
+    }
 }
 
 function ec_setIValue(winfo, targetInput, value) {
@@ -107,12 +99,12 @@ function ec_setIValue(winfo, targetInput, value) {
     if (targetInput) {
         if (targetInput.tagName == "INPUT") {
             if ((targetInput.type == 'radio')) {
-                if (value == '0'){
+                if (value == '0') {
                     value = false;
                 }
                 targetInput.checked = value;
 
-                if (value){
+                if (value) {
                     winfo.changeCheckClasses(targetInput, false);
                 }
             } else if ((targetInput.type == 'checkbox')) {
@@ -152,13 +144,13 @@ function ec_setIValue(winfo, targetInput, value) {
                 if (isMultiple != 'false') {
                     var valueToCheck = $.inArray(targetInput.options[k].value, values);
                     $.each(values, function (index, val) {
-                        if (targetInput.options[k].value == val){
+                        if (targetInput.options[k].value == val) {
                             targetInput.options[k].selected = true;
-                        } else if (valueToCheck < 0){
+                        } else if (valueToCheck < 0) {
                             targetInput.options[k].selected = false;
                         }
                     });
-                } else if (targetInput.options[k].value == value){
+                } else if (targetInput.options[k].value == value) {
                     targetInput.selectedIndex = k;
                 }
             }
@@ -171,42 +163,40 @@ function ec_setIValue(winfo, targetInput, value) {
     }
 }
 
-function ec_setIValuePlus(winfo,iid,v) {
-    var iid0= iid+'_0';
-    var i=0;
-    var oi=winfo.document.getElementById(iid0);
-    var ret=false;
+function ec_setIValuePlus(winfo, iid, v) {
+    var iid0 = iid + '_0';
+    var i = 0;
+    var ret = false;
     var isMultiple = false;
-    var elem = winfo.document.getElementById("sp_"+iid);
+    var elem = winfo.document.getElementById("sp_" + iid);
     if (elem) {
         isMultiple = elem.parentNode.parentNode.parentNode.parentNode.getAttribute("multiple");
     }
+    var oi = winfo.document.getElementById(iid0);
     while (oi) {
-        if (oi) {
-            if ((oi.type=='radio')||(oi.type=='checkbox')) {
-                if (isMultiple != 'false') {
-                    var values = v.split("\n");
-                    var valueToCheck = $.inArray(oi.value, values);
-                    $.each(values, function(index, val) {
-                        if (oi.value==val) {
-                            oi.checked=true;
-                            $(oi).trigger('click');
-                            ret=true;
-                        } else if (oi.checked == true && valueToCheck < 0){
-                            oi.checked = false;
-                            $(oi).trigger('click');
-                        }
-                    });
-                } else if (oi.value==v) {
-                    oi.checked=true;
-                    $(oi).trigger('click');
-                    ret=true;
-                }
+        if ((oi.type == 'radio') || (oi.type == 'checkbox')) {
+            if (isMultiple != 'false') {
+                var values = v.split("\n");
+                var valueToCheck = $.inArray(oi.value, values);
+                $.each(values, function (index, val) {
+                    if (oi.value == val) {
+                        oi.checked = true;
+                        $(oi).trigger('click');
+                        ret = true;
+                    } else if (oi.checked == true && valueToCheck < 0) {
+                        oi.checked = false;
+                        $(oi).trigger('click');
+                    }
+                });
+            } else if (oi.value == v) {
+                oi.checked = true;
+                $(oi).trigger('click');
+                ret = true;
             }
         }
         i++;
-        iid0=iid+'_'+i.toString();
-        oi=winfo.document.getElementById(iid0);
+        iid0 = iid + '_' + i.toString();
+        oi = winfo.document.getElementById(iid0);
     }
     if (!ret && v == " ") {
         ret = true;
