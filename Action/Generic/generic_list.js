8b0e1bfdf94fa4d346b9ad09148d80ac1d4be260
Éric Brison <eric.brison@anakeen.com>
2013-03-19 18:16:14 +0100

    change generic list search input (refs #3800)

--- Action/Generic/generic_list.js
+++ Action/Generic/generic_list.js
@@ -24,20 +24,85 @@
 
     reloadCurrentWindow = function reloadCurrentWindow(url) {
         if (url === "reload") {
-            window.location.href=window.location.href;
+            window.location.href = window.location.href;
         } else if (url) {
             window.location = url;
         }
     };
 
+    focusInput= function focusInput(event) {
+        $("#searchkey").focus();
+                            // set cursor at the end when reset value
+                            var iVal=document.getElementById('searchkey');
+                            var pVal=iVal.value;
+                            iVal.value='';
+                            iVal.value=pVal;
+           };
+
     $(document).on("ready", function () {
-        $("#mainbarmenu").on("click", "[target=_overlay]", function () {
-            var $this = $(this), href = $this.attr("url") || $this.attr("href");
-            if (tryAllParent(window, "openIframeOverlay", href, reloadCurrentWindow) === false) {
-                window.open(href);
+            $("#mainbarmenu").on("click", "[target=_overlay]", function () {
+                var $this = $(this), href = $this.attr("url") || $this.attr("href");
+                if (tryAllParent(window, "openIframeOverlay", href, reloadCurrentWindow) === false) {
+                    window.open(href);
+                }
+                return false;
+            });
+            $("#selectsearches")
+
+                .click(function () {
+                    var menu = $(this).next().show().position({
+                        my: "left top",
+                        at: "left bottom",
+                        of: this
+                    });
+                    $(document).one("click", function () {
+                        menu.hide();
+                    });
+                    return false;
+                })
+               // .buttonset()
+                .next()
+                .hide()
+                .menu();
+
+            $("#searches").on("click","a",function(event) {
+                $(this).parent().parent().find("a").attr("data-selected","0");
+
+                $(this).attr("data-selected","1");
+                if ($(this).attr("data-isreport")=="1") {
+                    // direct display reports
+                    SendSimpleSearch(event);
+                   $(this).attr("data-selected","0");
+                } else {
+                    // display selected search
+                    $("#selected-search-text").text($(this).text());
+                    $("#selected-search").show();
+
+                    focusInput();
+
+                }
+            });
+           /* $('#searchkey').button();
+            $('#searchgo').button({
+                text: false,
+                icons: {
+                    primary: "ui-icon-search"
+                }});*/
+            $("#selected-search-text").text($('a[data-selected="1"]').text());
+            if ($('a[data-selected="1"]').length > 0) {
+                $("#selected-search").show();
+            } else {
+                $("#selected-search").hide();
             }
-            return false;
-        });
-    });
+            $("#close-select-search").click(function() {
+                $('a[data-selected="1"]').attr("data-selected","0");
+                $("#selected-search").hide();
+
+            });
+            focusInput();
+        }
+
+
+    );
 
 }($, window));
