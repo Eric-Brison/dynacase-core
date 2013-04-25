bb493a288de11b472b24234f869470fff09fa548
Jérôme Augé <jerome.auge@anakeen.com>
2013-04-25 18:15:12 +0200

    Added PU_test_dcp_getDocAnchor.php (refs #3651)

---
+++ Test/control/PU_test_dcp_getDocAnchor.php
@@ -0,0 +1,94 @@
+<?php
+/*
+ * @author Anakeen
+ * @license http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License
+ * @package FDL
+*/
+
+namespace Dcp\Pu;
+/**
+ * @author Anakeen
+ * @license http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License
+ * @package Dcp\Pu
+ */
+
+require_once 'PU_testcase_dcp_commonfamily.php';
+require_once 'Lib.Main.php';
+
+class TestGetDocAnchor extends TestCaseDcpCommonFamily
+{
+    public $famName = "TST_TITLE";
+    /**
+     * import TST_TITLE family
+     * @static
+     * @return string
+     */
+    protected static function getCommonImportFile()
+    {
+        return "PU_data_dcp_getdocanchor.ods";
+    }
+    /**
+     * @dataProvider data_getDocAnchorMail
+     */
+    public function test_getDocAnchorMail($data)
+    {
+        $action = self::getAction();
+        
+        foreach ($data['params'] as $param) {
+            \ApplicationParameterManager::setCommonParameterValue($param['app'], $param['name'], $param['value']);
+        }
+        initMainVolatileParam($action->parent);
+        
+        $doc = new_Doc(self::$dbaccess, $data['doc']);
+        $this->assertTrue(is_object($doc) , sprintf("Could not get document with id '%s'.", $data['doc']));
+        
+        $data['expected_href'] = str_replace('%ID%', $doc->id, $data['expected_href']);
+        
+        $anchor = $doc->getDocAnchor($doc->id, 'mail');
+        $this->assertTrue(preg_match('/href=([\'"])(?P<href>.*?)\1/', $anchor, $m) === 1, sprintf("Could not find href='...' in anchor '%s'.", $anchor));
+        $href = $m['href'];
+        $this->assertTrue($href == $data['expected_href'], sprintf("Unexpected href '%s' (expecting '%s') in anchor '%s'.", $href, $data['expected_href'], $anchor));
+    }
+    
+    public function data_getDocAnchorMail()
+    {
+        return array(
+            array(
+                array(
+                    "doc" => "TST_GETDOCANCHOR_1",
+                    "params" => array(
+                        array(
+                            "app" => "CORE",
+                            "name" => "CORE_MAILACTION",
+                            "value" => ""
+                        ) ,
+                        array(
+                            "app" => "CORE",
+                            "name" => "CORE_URLINDEX",
+                            "value" => "http://www1.example.net/"
+                        )
+                    ) ,
+                    "expected_href" => "http://www1.example.net/?app=FDL&action=OPENDOC&mode=view&amp;id=%ID%&amp;latest=Y"
+                )
+            ) ,
+            array(
+                array(
+                    "doc" => "TST_GETDOCANCHOR_1",
+                    "params" => array(
+                        array(
+                            "app" => "CORE",
+                            "name" => "CORE_MAILACTION",
+                            "value" => "http://www2.example.net/?app=FOO&action=BAR"
+                        ) ,
+                        array(
+                            "app" => "CORE",
+                            "name" => "CORE_URLINDEX",
+                            "value" => ""
+                        )
+                    ) ,
+                    "expected_href" => "http://www2.example.net/?app=FOO&action=BAR&amp;id=%ID%&amp;latest=Y"
+                )
+            )
+        );
+    }
+}
