c0f62105ef84a7d36ef812a44e9faffe49a76a2e
Éric Brison <eric.brison@anakeen.com>
2013-06-27 09:38:05 +0200

    add DocFile Class to migrate (refs #4096)

---
+++ Class/Fdl/Class.DocFile.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * @author Anakeen
+ * @license http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License
+ * @package FDL
+*/
+
+/**
+ * Class DocFile
+ * @deprecated only need for migrated
+ */
+class DocFile extends \PDoc
+{
+    
+    var $defDoctype = 'F';
+    var $defClassname = '\Dcp\Family\Document';
+}
+?>
