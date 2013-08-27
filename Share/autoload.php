7f4de8c9ed7c32f2e6755a7324510f0a3b912aae
Jérôme Augé <jerome.auge@anakeen.com>
2013-08-27 09:49:03 +0200

    Changed autoloader-ignore mechanism (refs #4186)
    - Exclusion globs are now specified in files located in the
      `$wpub/.autoloader-ignore.d` directory.
    - `.autoloader-ignore` files are not interpreted anymore, and they
      must be migrated to the `.autoloader-ignore.d` format.

--- Share/autoload.php
+++ Share/autoload.php
@@ -10,8 +10,7 @@ include_once 'WHAT/Lib.Prefix.php';
 
 class excludeSomeFiles extends FilterIterator
 {
-    private $_ignoreFileCache = array();
-    private $_ignoreDirCache = array();
+    private $_ignoreGlobList = false;
     
     public function accept()
     {
@@ -37,8 +36,11 @@ class excludeSomeFiles extends FilterIterator
         
         return is_readable($c);
     }
+    private function _inAutoloaderIgnoreDotD($fileName)
+    {
+    }
     /**
-     * Check path components for .autoloader-ignore files and
+     * Check filname againts glob patterns from .autoloader-ignore.d files and
      * evaluate if the given (file|dir)ame should be ignored.
      *
      * @param string $fileName The filename to check
@@ -46,82 +48,81 @@ class excludeSomeFiles extends FilterIterator
      */
     private function _inAutoloaderIgnore($fileName)
     {
-        $pathList = explode('/', $fileName);
-        for ($p = 1; $p < count($pathList); $p++) {
-            $pathName = join('/', array_slice($pathList, 0, $p + 1));
-            /* Check if this directory has already been checked
-             * and use the previously evaluated status
-            */
-            if (array_key_exists($pathName, $this->_ignoreDirCache)) {
-                if ($this->_ignoreDirCache[$pathName]) {
-                    return true;
-                } else {
-                    continue;
-                }
-            }
-            /* Check for .autoloader-ignore file in the parent dir */
-            $baseName = basename($pathName);
-            $dirName = dirname($pathName);
-            $ignoreFile = $this->_getAutoloaderIgnoreFile($dirName);
-            if ($ignoreFile === false) {
-                continue;
-            }
-            /* Evaluate if the basename is to be ignored */
-            foreach ($ignoreFile as $glob) {
-                if (fnmatch($glob, $baseName)) {
-                    if (is_dir($pathName)) {
-                        $this->_ignoreDirCache[$pathName] = true;
-                    }
-                    return true;
-                }
-            }
-            if (is_dir($pathName)) {
-                $this->_ignoreDirCache[$pathName] = false;
+        if ($this->_ignoreGlobList === false) {
+            $this->_ignoreGlobList = $this->_parseAutoloaderIgnoreDotD();
+        }
+        if (strpos($fileName, './') === 0) {
+            $fileName = substr($fileName, 2);
+        }
+        foreach ($this->_ignoreGlobList as $glob) {
+            if (fnmatch($glob, $fileName)) {
+                return true;
             }
         }
         return false;
     }
     /**
-     * Check for, and parse, a .autoloader-ignore file in the given directory
-     *
-     * @param string $dirName The directory
-     * @return array|bool bool(false) on error, list of globs (from the .autoloader-ignore file) on success
-     */
-    private function _getAutoloaderIgnoreFile($dirName)
-    {
-        if (isset($this->_ignoreFileCache[$dirName])) {
-            return $this->_ignoreFileCache[$dirName];
-        }
-        if (!is_dir($dirName)) {
-            false;
-        }
-        $ignoreFile = $dirName . DIRECTORY_SEPARATOR . '.autoloader-ignore';
-        if (!file_exists($ignoreFile)) {
-            return $this->_ignoreFileCache[$dirName] = false;
-        }
-        /* Cache and return the parsed ignore list */
-        return $this->_ignoreFileCache[$dirName] = $this->_parseAutoloaderIgnoreFile($ignoreFile);
-    }
-    /**
      * Load and parse a .autoloader-ignore file returning the list of globs.
      *
      * @param string $ignoreFile Path to the .autoloader-ignore file to parse
+     * @throws Dcp\DirectoriesAutoloaderException
      * @return array|bool bool(false) on error, list of glob patterns on success
      */
     private function _parseAutoloaderIgnoreFile($ignoreFile)
     {
         $globList = file($ignoreFile);
         if ($globList === false) {
-            return false;
+            throw new \Dcp\DirectoriesAutoloaderException(sprintf("Error opening '.autoloader-ignore.d' directory '%s'.", $dir));
         }
         $globList = array_filter(array_map(function ($e)
         {
+            /* Remove trailing CR+LF */
             return rtrim($e, "\r\n");
         }
         , $globList) , function ($e)
         {
+            /* Skip blank and comment lines */
             return !preg_match('/^\s*(#.*)?$/', $e);
         });
+        $globList = array_map(function ($e)
+        {
+            /*
+             * Strip leading './' from glob patterns as they are
+             * already considered relatives to the current directory.
+            */
+            if (strpos($e, './') === 0) {
+                $e = substr($e, 2);
+            }
+            return $e;
+        }
+        , $globList);
+        return $globList;
+    }
+    /**
+     * Load and parse ignore files from `$wpub/.autoloader-ignore.d` dir
+     *
+     * @throws Dcp\DirectoriesAutoloaderException
+     * @return array|bool bool(false) on error, or list of glob patterns on success
+     */
+    private function _parseAutoloaderIgnoreDotD()
+    {
+        global $pubdir;
+        $globList = array();
+        $dir = $pubdir . DIRECTORY_SEPARATOR . '.autoloader-ignore.d';
+        if (!is_dir($dir)) {
+            return $globList;
+        }
+        $dh = opendir($dir);
+        if ($dh === false) {
+            throw new \Dcp\DirectoriesAutoloaderException(sprintf("Error opening '.autoloader-ignore.d' directory '%s'.", $dir));
+        }
+        while ($file = readdir($dh)) {
+            if ($file == '.' || $file == '..') {
+                continue;
+            }
+            $globList = array_merge($globList, $this->_parseAutoloaderIgnoreFile($dir . DIRECTORY_SEPARATOR . $file));
+        }
+        closedir($dh);
         return $globList;
     }
 }
