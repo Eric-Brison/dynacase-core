295981091c6ac5096d755c2796fa22030ee7ccfa
Éric Brison <eric.brison@anakeen.com>
2013-11-20 17:37:39 +0100

    optimize seems_utf8 (refs #4304)

--- Class/Vault/Class.VaultDiskStorage.php
+++ Class/Vault/Class.VaultDiskStorage.php
@@ -153,17 +153,7 @@ class VaultDiskStorage extends DbObj
     
     function seems_utf8($Str)
     {
-        for ($i = 0; $i < strlen($Str); $i++) {
-            if (ord($Str[$i]) < 0x80) $n = 0; # 0bbbbbbb
-            elseif ((ord($Str[$i]) & 0xE0) == 0xC0) $n = 1; # 110bbbbb
-            elseif ((ord($Str[$i]) & 0xF0) == 0xE0) $n = 2; # 1110bbbb
-            elseif ((ord($Str[$i]) & 0xF0) == 0xF0) $n = 3; # 1111bbbb
-            else return false; # Does not match any model
-            for ($j = 0; $j < $n; $j++) { # n octets that match 10bbbbbb follow ?
-                if ((++$i == strlen($Str)) || ((ord($Str[$i]) & 0xC0) != 0x80)) return false;
-            }
-        }
-        return true;
+        return preg_match('!!u', $Str);
     }
     /**
      * Add new file in VAULT
