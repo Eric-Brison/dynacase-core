Eric-Brison/dynacase-core

commit 3e47c82708155949b5059909cebd375eb53f19f0

use only one session variable dcpsession (refs #3894)

Class/Session/Class.Session.php
@@ -79,7 +79,7 @@ class Session extends DbObj
     
     var $sessiondb;
     
-    const PARAMNAME = 'freedom_param';
+    const PARAMNAME = 'dcpsession';
     var $session_name = self::PARAMNAME;
     function __construct($session_name = self::PARAMNAME)
     {
@@ -110,7 +110,7 @@ class Session extends DbObj
                 session_name($this->session_name);
                 session_id($id);
                 @session_start();
-                @session_write_close(); // avoid block
+                //@session_write_close(); // avoid block
                 
             }
         }
@@ -159,7 +159,7 @@ class Session extends DbObj
             session_id($this->id);
             @session_unset();
             @session_destroy();
-            @session_write_close();
+            //@session_write_close();
             // delete session cookie
             setcookie($this->name, false, time() - 3600);
             $this->Delete();
@@ -199,7 +199,7 @@ class Session extends DbObj
             session_name($this->session_name);
             session_id($idsess);
             @session_start();
-            @session_write_close(); // avoid block
+            //@session_write_close(); // avoid block
             //	$this->initCache();
             
         }
@@ -226,11 +226,11 @@ class Session extends DbObj
         global $_SERVER; // use only cache with HTTP
         if (!empty($_SERVER['HTTP_HOST'])) {
             //	session_register($k);
-            session_name($this->name);
-            session_id($this->id);
-            @session_start();
+            // session_name($this->name);
+            // session_id($this->id);
+            // @session_start();
             $_SESSION[$k] = $v;
-            @session_write_close(); // avoid block
+            //@session_write_close(); // avoid block
             
         }
         
@@ -243,19 +243,24 @@ class Session extends DbObj
     function Read($k = "", $d = "")
     {
         if (!empty($_SERVER['HTTP_HOST'])) {
-            session_name($this->name);
-            session_id($this->id);
-            @session_start();
+            //session_name($this->name);
+            //session_id($this->id);
+            //@session_start();
             if (isset($_SESSION[$k])) {
                 $val = $_SESSION[$k];
-                @session_write_close();
+                //@session_write_close();
                 return $val;
             } else {
-                @session_write_close();
+                //@session_write_close();
                 return ($d);
             }
         }
         return ($d);
+    }
+    
+    private function logDebug($s)
+    {
+        error_log(sprintf("session %s:%s", posix_getpid() , $s));
     }
     // --------------------------------
     // Détruit une variable de session
@@ -265,11 +270,11 @@ class Session extends DbObj
     {
         global $_SERVER; // use only cache with HTTP
         if (!empty($_SERVER['HTTP_HOST'])) {
-            session_name($this->name);
-            session_id($this->id);
-            @session_start();
+            // session_name($this->name);
+            // session_id($this->id);
+            // @session_start();
             unset($_SESSION[$k]);
-            @session_write_close(); // avoid block
+            //@session_write_close(); // avoid block
             
         }
         return true;
@@ -295,10 +300,9 @@ class Session extends DbObj
     {
         global $_SERVER; // use only cache with HTTP
         if (!empty($_SERVER['HTTP_HOST'])) {
-            
-            session_name($this->name);
-            session_id($this->id);
-            @session_start();
+            // session_name($this->name);
+            // session_id($this->id);
+            // @session_start();
             foreach ($_SESSION as $k => $v) {
                 if (preg_match("/^sessparam[0-9]+$/", $k)) {
                     if (isset($v[$paramName])) {
@@ -306,7 +310,7 @@ class Session extends DbObj
                     }
                 }
             }
-            @session_write_close(); // avoid block
+            //@session_write_close(); // avoid block
             
         }
         return true;

Class/Appmng/Class.Application.php
@@ -235,18 +235,20 @@ create sequence SEQ_ID_APPLICATION start 10;
         }
         
         if ($session != "") $this->SetSession($session);
-        $sessparam = false;
         $this->param = new Param($this->dbaccess);
-        if ($this->session) $sessparam = $this->session->read("sessparam" . $this->id, false);
-        if ($sessparam) {
-            $this->param->appid = $this->id;
-            $this->param->buffer = $sessparam;
-            $this->InitStyle(false);
+        $style = false;
+        if ($this->session) $style = $this->session->read("userCoreStyle", false);
+        
+        if ($style) {
+            $this->InitStyle(false, $style);
         } else {
             $this->InitStyle();
-            $this->param->SetKey($this->id, isset($this->user->id) ? $this->user->id : false, $this->style->name);
-            if ($this->session) $this->session->register("sessparam" . $this->id, $this->param->buffer);
         }
+        if ((!$style) && $this->session) {
+            $this->session->register("userCoreStyle", $this->getParam("STYLE"));
+        }
+        
+        $this->param->SetKey($this->id, isset($this->user->id) ? $this->user->id : false, $this->style->name);
         if (!$this->rootdir) $this->rootdir = $this->Getparam("CORE_PUBDIR");
         if ($this->available == "N") {
             // error
@@ -819,7 +821,7 @@ create sequence SEQ_ID_APPLICATION start 10;
      * create style parameters
      * @param bool $init
      */
-    public function initStyle($init = true)
+    public function initStyle($init = true, $useStyle = '')
     {
         if ($init == true) {
             if (isset($this->user)) $pstyle = new Param($this->dbaccess, array(
@@ -843,15 +845,17 @@ create sequence SEQ_ID_APPLICATION start 10;
             
             $this->style->Set($this);
         } else {
-            $style = $this->getParam("STYLE");
+            $style = ($useStyle) ? $useStyle : $this->getParam("STYLE");
             $this->style = new Style($this->dbaccess, $style);
             
             $this->style->Set($this);
         }
-        if ("Y" == $this->style->parsable) {
-            $this->AddCssRef("$style:gen.css", true);
-        } else {
-            $this->AddCssRef("STYLE/$style/Layout/gen.css");
+        if ($style) {
+            if ("Y" == $this->style->parsable) {
+                $this->AddCssRef("$style:gen.css", true);
+            } else {
+                $this->AddCssRef("STYLE/$style/Layout/gen.css");
+            }
         }
         $size = $this->getParam("FONTSIZE", "normal");
         $this->AddCssRef("WHAT/Layout/size-$size.css");

Class/Appmng/Class.ApplicationParameterManager.php
@@ -159,17 +159,6 @@ class ApplicationParameterManager
             throw new \Dcp\ApplicationParameterManager\Exception("APM0006", $applicationId, $parameterName, $err);
         }
         self::$cache[$applicationId . ' ' . $parameterName . ' ' . $userId] = $value;
-        
-        if ($action) {
-            $action->parent->session->closeAll($userId);
-            if ($action->parent->session->isAffected()) {
-                $session = $action->parent->session;
-            } else {
-                $session = new Session(Session::PARAMNAME);
-            }
-            
-            $session->closeAll($userId);
-        }
     }
     /**
      * Set the user parameter default value
@@ -217,7 +206,7 @@ class ApplicationParameterManager
         } else {
             $parameter = new Param(getDbAccess());
         }
-
+        
         $type = ($isGlobal === "G") ? PARAM_GLB : PARAM_APP;
         
         $err = $parameter->set($parameterName, $value, $type, $applicationId);
@@ -226,16 +215,6 @@ class ApplicationParameterManager
             throw new \Dcp\ApplicationParameterManager\Exception("APM0009", $parameterName, $applicationId, $err);
         }
         self::$cache[$applicationId . ' ' . $parameterName] = $value;
-        
-        if ($action) {
-            $action->parent->session->closeAll();
-            if ($action->parent->session->isAffected()) {
-                $session = $action->parent->session;
-            } else {
-                $session = new Session(Session::PARAMNAME);
-            }
-            $session->closeAll();
-        }
     }
     /**
      * Get a parameter value in the database

Class/Appmng/Class.ParameterManager.php
@@ -195,16 +195,6 @@ class ParameterManager
         if (isset(self::$cache[$appName][$name])) self::$cache[$appName][$name] = $value;
         
         $a = self::getAction();
-        if ($a) {
-            $a->parent->session->closeAll();
-            if ($a->parent->session->isAffected()) {
-                $s = $a->parent->session;
-            } else {
-                $s = new Session(Session::PARAMNAME);
-            }
-            
-            $s->closeAll();
-        }
     }
     private static function setUserApplicationTypeParameter($userId, $appName, $appId, $name, $value)
     {
@@ -225,17 +215,5 @@ class ParameterManager
             throw new \Dcp\PMGT\Exception("PMGT0005", $name, $appName, $err);
         }
         if (isset(self::$cache[$appName][$name])) self::$cache[$appName][$name] = $value;
-        
-        $a = self::getAction();
-        if ($a) {
-            $a->parent->session->closeAll($userId);
-            if ($a->parent->session->isAffected()) {
-                $s = $a->parent->session;
-            } else {
-                $s = new Session(Session::PARAMNAME);
-            }
-            
-            $s->closeAll($userId);
-        }
     }
 }

Class/Database/Class.Param.php
@@ -95,15 +95,6 @@ class Param extends DbObj
         if ($pdef->isAffected()) {
             if ($pdef->isglob == 'Y') {
                 $appid = $pdef->appid;
-                if ($action) {
-                    if ($type[0] != PARAM_USER) {
-                        $action->parent->session->close(); // need to refresh all application parameters
-                        $action->parent->session->set(); // reopen current session
-                        
-                    } else {
-                        $action->parent->session->replaceGlobalParam($name, $val);
-                    }
-                }
             }
         }
         $this->appid = $appid;
@@ -156,20 +147,21 @@ class Param extends DbObj
             PARAM_USER . $userid,
             "1"
         ));
+        $out = array();
         if ($psize->val != '') $size = $psize->val;
         else $size = 'normal';
         $size = 'SIZE_' . strtoupper($size);
-        $query = new QueryDb($this->dbaccess, "Param");
+        
         if ($appid) {
             if ($userid) {
-                $list = $query->Query(0, 0, "TABLE", "select distinct on(paramv.name) paramv.* from paramv left join paramdef on (paramv.name=paramdef.name) where " . "(paramv.type = '" . PARAM_GLB . "') " . " OR (paramv.type='" . PARAM_APP . "' and paramv.appid=$appid)" . " OR (paramv.type='" . PARAM_USER . $userid . "' and paramv.appid=$appid)" . " OR (paramv.type='" . PARAM_USER . $userid . "' and paramdef.isglob='Y')" . " OR (paramv.type='" . PARAM_STYLE . $styleid . "' and paramv.appid=$appid)" . " OR (paramv.type='" . PARAM_STYLE . $styleid . "' and paramdef.isglob='Y')" . " OR (paramv.type='" . PARAM_STYLE . $size . "')" . " order by paramv.name, paramv.type desc");
+                $sql = "select distinct on(paramv.name) paramv.* from paramv left join paramdef on (paramv.name=paramdef.name) where " . "(paramv.type = '" . PARAM_GLB . "') " . " OR (paramv.type='" . PARAM_APP . "' and paramv.appid=$appid)" . " OR (paramv.type='" . PARAM_USER . $userid . "' and paramv.appid=$appid)" . " OR (paramv.type='" . PARAM_USER . $userid . "' and paramdef.isglob='Y')" . " OR (paramv.type='" . PARAM_STYLE . $styleid . "' and paramv.appid=$appid)" . " OR (paramv.type='" . PARAM_STYLE . $styleid . "' and paramdef.isglob='Y')" . " OR (paramv.type='" . PARAM_STYLE . $size . "')" . " order by paramv.name, paramv.type desc";
             } else {
-                $list = $query->Query(0, 0, "TABLE", sprintf("SELECT * from paramv where type='G' or (type='A' and appid=%d);", $appid));
+                $sql = sprintf("SELECT * from paramv where type='G' or (type='A' and appid=%d);", $appid);
             }
-        }
-        $out = array();
-        if ($query->nb != 0) {
-            while (list($k, $v) = each($list)) {
+            
+            simpleQuery($this->dbaccess, $sql, $list);
+            
+            foreach ($list as $v) {
                 $out[$v["name"]] = $v["val"];
             }
         } else {

Config/dbaccess.php.in
@@ -22,7 +22,7 @@ $freedom_authtype = '@AUTHTYPE@';
 
 $freedom_authtypeparams = array(
     'html' => array(
-        'cookie' => 'freedom_auth',
+        'cookie' => 'dcpsession',
         'auth' => array(
             'app' => 'AUTHENT',
             'action' => 'LOGINFORM',

Share/Lib.Main.php
@@ -49,14 +49,17 @@ function getMainAction($auth, &$action)
         }
     }
     
-    if (isset($_COOKIE[Session::PARAMNAME])) $sess_num = $_COOKIE[Session::PARAMNAME];
-    else $sess_num = GetHttpVars(Session::PARAMNAME); //$_GET["session"];
-    $session = new Session();
-    if (!$session->Set($sess_num)) {
-        print "<strong>:~((</strong>";
-        exit;
-    };
-    
+    if (isset($auth->auth_session)) {
+        $session = $auth->auth_session;
+    } else {
+        $session = new Session();
+        if (isset($_COOKIE[Session::PARAMNAME])) $sess_num = $_COOKIE[Session::PARAMNAME];
+        else $sess_num = GetHttpVars(Session::PARAMNAME); //$_GET["session"];
+        if (!$session->Set($sess_num)) {
+            print "<strong>:~((</strong>";
+            exit;
+        };
+    }
     $core = new Application();
     $core->Set("CORE", $CoreNull, $session);
     

pack.php
@@ -31,7 +31,7 @@ function exitError($text)
     exit;
 }
 
-$cookName = 'freedom_param'; // see Session::PARAMNAME .. not included here to be independant
+$cookName = 'dcpsession'; // see Session::PARAMNAME .. not included here to be independant
 if (!isset($_COOKIE[$cookName])) exitError('Not connected');
 $sessid = $_COOKIE[$cookName];
 

commit 0b5b9512afa60db079aaaad19e5e0cd24bc7402d

fix(Session): Revert to non-blocking sessions (refs #4825)
Commit 3e47c82708155949b5059909cebd375eb53f19f0 introduced a regression
that blocked concurrent requests with same session id.

This reverts to the original non-blocking behaviour.

Class/Session/Class.Session.php
@@ -112,7 +112,7 @@ class Session extends DbObj
                 session_name($this->session_name);
                 session_id($id);
                 @session_start();
-                //@session_write_close(); // avoid block
+                @session_write_close(); // avoid block
                 
             }
         }
@@ -150,7 +150,7 @@ class Session extends DbObj
             setcookie($this->name, $id, $ttl);
         }
     }
-    /** 
+    /**
      * Closes session and removes all datas
      */
     function Close()
@@ -161,7 +161,7 @@ class Session extends DbObj
             session_id($this->id);
             @session_unset();
             @session_destroy();
-            //@session_write_close();
+            @session_write_close();
             // delete session cookie
             setcookie($this->name, false, time() - 3600);
             $this->Delete();
@@ -169,7 +169,7 @@ class Session extends DbObj
         $this->status = self::SESSION_CT_CLOSE;
         return $this->status;
     }
-    /** 
+    /**
      * Closes all session
      */
     function CloseAll($uid = null)
@@ -182,7 +182,7 @@ class Session extends DbObj
         $this->status = self::SESSION_CT_CLOSE;
         return $this->status;
     }
-    /** 
+    /**
      * Closes all user's sessions
      */
     function CloseUsers($uid = - 1)
@@ -201,7 +201,7 @@ class Session extends DbObj
             session_name($this->session_name);
             session_id($idsess);
             @session_start();
-            //@session_write_close(); // avoid block
+            @session_write_close(); // avoid block
             //	$this->initCache();
             
         }
@@ -224,11 +224,12 @@ class Session extends DbObj
         }
         global $_SERVER; // use only cache with HTTP
         if (!empty($_SERVER['HTTP_HOST'])) {
-            if (!isset($_SESSION[$k]) ||  $_SESSION[$k] !== $v) {
-                $_SESSION[$k] = $v;
-                session_write_close(); // avoid block
-                session_start();
-            }
+            session_name($this->name);
+            session_id($this->id);
+            @session_start();
+            $_SESSION[$k] = $v;
+            @session_write_close(); // avoid block
+            
         }
         
         return true;
@@ -239,20 +240,26 @@ class Session extends DbObj
     // --------------------------------
     function Read($k = "", $d = "")
     {
-        if (!empty($_SERVER['HTTP_HOST'])) {
-            //session_name($this->name);
-            //session_id($this->id);
-            //@session_start();
-            if (isset($_SESSION[$k])) {
-                $val = $_SESSION[$k];
-                //@session_write_close();
-                return $val;
-            } else {
-                //@session_write_close();
-                return ($d);
-            }
+        if (empty($_SERVER['HTTP_HOST'])) {
+            return ($d);
         }
-        return ($d);
+        /* Load session's data only once as requested by #4825 */
+        $sessionOpened = false;
+        if (!isset($_SESSION)) {
+            session_name($this->name);
+            session_id($this->id);
+            @session_start();
+            $sessionOpened = true;
+        }
+        if (isset($_SESSION[$k])) {
+            $val = $_SESSION[$k];
+        } else {
+            $val = $d;
+        }
+        if ($sessionOpened) {
+            @session_write_close();
+        }
+        return $val;
     }
     // --------------------------------
     // Détruit une variable de session
@@ -262,11 +269,11 @@ class Session extends DbObj
     {
         global $_SERVER; // use only cache with HTTP
         if (!empty($_SERVER['HTTP_HOST'])) {
-            // session_name($this->name);
-            // session_id($this->id);
-            // @session_start();
+            session_name($this->name);
+            session_id($this->id);
+            @session_start();
             unset($_SESSION[$k]);
-            //@session_write_close(); // avoid block
+            @session_write_close(); // avoid block
             
         }
         return true;
@@ -292,9 +299,9 @@ class Session extends DbObj
     {
         global $_SERVER; // use only cache with HTTP
         if (!empty($_SERVER['HTTP_HOST'])) {
-            // session_name($this->name);
-            // session_id($this->id);
-            // @session_start();
+            session_name($this->name);
+            session_id($this->id);
+            @session_start();
             foreach ($_SESSION as $k => $v) {
                 if (preg_match("/^sessparam[0-9]+$/", $k)) {
                     if (isset($v[$paramName])) {
@@ -302,7 +309,7 @@ class Session extends DbObj
                     }
                 }
             }
-            //@session_write_close(); // avoid block
+            @session_write_close(); // avoid block
             
         }
         return true;