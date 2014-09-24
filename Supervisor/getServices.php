2a324d9069f2c8f0b487fd1bb5485ea6a2088931
Éric Brison <eric.brison@anakeen.com>
2014-09-24 14:23:53 +0200

    fix(strict): set service method as static (refs #4921)

--- Supervisor/getServices.php
+++ Supervisor/getServices.php
@@ -1,53 +1,63 @@
 <?php
+/*
+ * @author Anakeen
+ * @license http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License
+ * @package FDL
+ */
 
-class Service {
-
-  private static $path = "./service.d/";
-
-  function serviceInfos($path) {
-    $info = null;
-    if (is_file($path."/info.php")) {
-      include($path."/info.php");
-      $info["error"] = "";
-      $info["title"] = (isset($title) ? $title : $path);
-      $info["description"] = (isset($description) ? $description : $path);
-      if (!isset($start)) {
-         $info["error"] = "Can't load service {$path}, start is not defined.";
-      } else {
-        if (is_file($path."/".$start)) {
-           $info["start"] = $path."/".$start;
+class Service
+{
+    
+    private static $path = "./service.d/";
+    
+    public static function serviceInfos($path)
+    {
+        $info = null;
+        if (is_file($path . "/info.php")) {
+            include ($path . "/info.php");
+            $info["error"] = "";
+            $info["title"] = (isset($title) ? $title : $path);
+            $info["description"] = (isset($description) ? $description : $path);
+            if (!isset($start)) {
+                $info["error"] = "Can't load service {$path}, start is not defined.";
+            } else {
+                if (is_file($path . "/" . $start)) {
+                    $info["start"] = $path . "/" . $start;
+                } else {
+                    $info["error"] = "Can't load service {$path}, start file {$start} not found.";
+                }
+            }
         } else {
-           $info["error"] = "Can't load service {$path}, start file {$start} not found.";
+            $info["error"] = "Can't load service {$path}, info.php not found.";
         }
-      }
-    } else {
-      $info["error"] = "Can't load service {$path}, info.php not found.";
+        return $info;
     }
-    return $info;
-  }
-
-  function getAll() {
-    $services = array();
-    if (is_dir(Service::$path)) {
-      if ($dh = opendir(Service::$path)) {
-        while (($file = readdir($dh)) !== false) {
-	  if ($file!="." && $file!=".." && filetype(Service::$path . $file)=="dir") {
-	    $ret = Service::serviceInfos(Service::$path.$file);
-	    if (is_array($ret)) $services[] = $ret;
-	  }
+    
+    public static function getAll()
+    {
+        $services = array();
+        if (is_dir(Service::$path)) {
+            if ($dh = opendir(Service::$path)) {
+                while (($file = readdir($dh)) !== false) {
+                    if ($file != "." && $file != ".." && filetype(Service::$path . $file) == "dir") {
+                        $ret = Service::serviceInfos(Service::$path . $file);
+                        if (is_array($ret)) $services[] = $ret;
+                    }
+                }
+                closedir($dh);
+            }
+            usort($services, 'Service::servicesort');
+        } else {
+            header("HTTP/1.0 404 Service directory not found");
         }
-        closedir($dh);
-      }
-      usort($services, 'Service::servicesort');
-    } else {
-      header("HTTP/1.0 404 Service directory not found");
+        return $services;
     }
-    return $services;
-  }
-
-  function servicesort($a, $b) { return strcmp($a["title"], $b["title"]); }
+    
+    public static function servicesort($a, $b)
+    {
+        return strcmp($a["title"], $b["title"]);
+    }
 }
 
 print_r(json_encode(Service::getAll()));
-
 ?>
