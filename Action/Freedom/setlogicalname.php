11e447bf659d1ae95f8e60bab71ff17b099c43cd
Jérôme Augé <jerome.auge@anakeen.com>
2014-12-16 11:05:35 +0100

    fix(setLogicalName): Change test of $name variable in FREEDOM:SETLOGICALNAME (refs #5254)

--- Action/Freedom/setlogicalname.php
+++ Action/Freedom/setlogicalname.php
@@ -23,15 +23,16 @@ function setlogicalname(Action & $action)
     $dbaccess = $action->GetParam("FREEDOM_DB");
     $docid = GetHttpVars("id");
     $name = GetHttpVars("name");
-    if ($docid && $name) {
+    if ($docid) {
         $doc = new_Doc($dbaccess, $docid, true);
         if (!$doc->isAlive()) {
             $action->addWarningMsg(sprintf(_("Document %s is not alive") , $doc->getTitle()));
         } else {
             $oldName = $doc->name;
             $err = $doc->setLogicalName($name, true);
-            if ($err != "") $action->addWarningMsg($err);
-            else {
+            if ($err != "") {
+                $action->addWarningMsg($err);
+            } elseif (!empty($name)) {
                 if ($oldName) {
                     $doc->addHistoryEntry(sprintf(_("update logical name from %s to %s") , $oldName, $doc->name));
                 } else {
@@ -42,4 +43,3 @@ function setlogicalname(Action & $action)
     }
     redirect($action, "FDL", "IMPCARD&zone=FDL:VIEWPROPERTIES:T&id=" . $docid);
 }
-?>
