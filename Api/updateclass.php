3b44038be8904cc9ba92ba47ce8ed5d35dad3ca2
Jérôme Augé <jerome.auge@anakeen.com>
2017-06-07 11:32:31 +0200

    fix(updateclass): Enhance `updateclass` API script (refs #6994)
    Enhancements:
    - Perform DB modifications in a transaction (so they can be rolled-back
      on failure).
    - Delete and recreate indexes associated with table.
    - Throw error when anything goes wrong (and revert DB changes).

--- Api/updateclass.php
+++ Api/updateclass.php
@@ -17,76 +17,100 @@
 /**
  */
 
+global $action;
+
 include_once ('Class.Application.php');
 
 $usage = new ApiUsage();
 
 $usage->setDefinitionText("Update the SQL structure of a table of a DbObj Object");
-$appclass = $usage->addOptionalParameter('appc', "application class folder", null, 'WHAT');
-$class = $usage->addRequiredParameter('class', 'Class name');
+$appClass = $usage->addOptionalParameter('appc', "application class folder", function ($value, $name)
+{
+    if (!is_scalar($value)) {
+        return sprintf("Multiple values for '%s' not allowed.", $name);
+    }
+    if (strpos($value, DIRECTORY_SEPARATOR) !== false) {
+        return sprintf("Value for '%s' must not contain directory separator chars ('%s').", $name, DIRECTORY_SEPARATOR);
+    }
+    return '';
+}
+, 'WHAT');
+$class = $usage->addRequiredParameter('class', 'Class name', function ($value, $name)
+{
+    if (!is_scalar($value)) {
+        return sprintf("Multiple values for '%s' not allowed.", $name);
+    }
+    if (!preg_match('/^[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*$/', $value)) {
+        return sprintf("Invalid class name '%s' for '%s'.", $value, $name, DIRECTORY_SEPARATOR);
+    }
+    return '';
+});
 $db = $usage->addOptionalParameter('dbcoord', "Database name", null, getDbAccess());
 
 $usage->verify();
 
-$phpClass = sprintf("%s/%s/Class.%s.php", DEFAULT_PUBDIR, $appclass, $class);
+$phpClass = sprintf("%s/%s/Class.%s.php", DEFAULT_PUBDIR, $appClass, $class);
 
-include_once ($phpClass);
+require_once ($phpClass);
 /**
  * @var DbObj $o
  */
 $o = new $class($db);
 
-$dbid = pg_connect($db);
-if (!$dbid) {
-    print _("cannot access to  database $db\n");
-    exit(1);
-} else {
-    print _("access granted to  database $db\n");
-}
-
 $sql = array();
-$rq = @pg_query($dbid, "select * from " . $o->dbtable . " LIMIT 1;");
-if (!$rq) {
-    // table not exist : just create
-    $o->Create();
-} else {
-    $row = 0;
-    
-    if (pg_result_error($rq) == "") {
-        if (pg_num_rows($rq) > 0) {
-            $row = pg_fetch_array($rq, 0, PGSQL_ASSOC);
-            if ($row) {
-                $fieds = array_intersect($o->fields, array_keys($row));
-                $sql[] = "CREATE TABLE " . $o->dbtable . "_old AS SELECT * FROM " . $o->dbtable . ";";
+$updateExistingTable = \Dcp\Core\PgInformationSchema::tableExists($o->dbaccess, 'public', $o->dbtable);
+if ($updateExistingTable) {
+    /* Compute columns that appears both in new and old table */
+    $columns = \Dcp\Core\PgInformationSchema::getTableColumns($o->dbaccess, 'public', $o->dbtable);
+    $commonColumns = array_intersect($o->fields, $columns);
+    /* Add SQL rename of current table to table + '_old' */
+    $oldTableName = sprintf("%s_old", $o->dbtable);
+    $sql[] = sprintf("ALTER TABLE public.%s RENAME TO %s", pg_escape_identifier($o->dbtable) , pg_escape_identifier($oldTableName));
+    /* Add SQL creation of new table */
+    $sqlCommands = explode(";", str_replace("\n", " ", $o->sqlcreate));
+    foreach ($sqlCommands as $k => $sqlQuery) {
+        $tableIndexes = \Dcp\Core\PgInformationSchema::getTableIndexes($o->dbaccess, 'public', $o->dbtable);
+        /* Drop index (if exists) before recreating it */
+        if (preg_match('/CREATE\s+  (?:UNIQUE\s+)?  INDEX\s+  (?:CONCURRENTLY\s+)?  (?:IF\s+NOT\s+EXISTS\s+)?  (?P<indexName>[a-z0-9_]+)/xi', $sqlQuery, $m)) {
+            /* If index exists, then drop it */
+            if (in_array($m['indexName'], $tableIndexes)) {
+                $sql[] = sprintf("DROP INDEX public.%s", pg_escape_identifier($m['indexName']));
             }
         }
-        $sql[] = "DROP TABLE " . $o->dbtable . ";";
+        if (chop($sqlQuery) != "") $sql[] = $sqlQuery;
     }
-    $sqlcmds = explode(";", $o->sqlcreate);
-    foreach ($sqlcmds as $k => $sqlquery) {
-        if (chop($sqlquery) != "") $sql[] = $sqlquery;
-    }
-    
-    if ($row) {
-        $sql[] = "INSERT INTO " . $o->dbtable . " (" . implode(",", $o->fields) . ") SELECT " . implode(",", $o->fields) . " FROM " . $o->dbtable . "_old";
-        
-        $sql[] = "DROP TABLE " . $o->dbtable . "_old;";
-    }
+    /* Add SQL to load common columns data from old table */
+    $sql[] = sprintf("INSERT INTO public.%s (%s) SELECT %s FROM public.%s", pg_escape_identifier($o->dbtable) , implode(", ", array_map('pg_escape_identifier', $commonColumns)) , implode(", ", array_map('pg_escape_identifier', $commonColumns)) , pg_escape_identifier($oldTableName));
+    /* Drop old table */
+    $sql[] = sprintf("DROP TABLE public.%s", pg_escape_identifier($oldTableName));
 }
-foreach ($sql as $k => $v) {
-    print "Sql:$v\n";
-    $rq = @pg_exec($dbid, $v);
-    if (!$rq) {
-        if (preg_match("/create sequence/", $v, $reg)) {
-            $pgmess = pg_errormessage($dbid);
-            echo "[1;33;49m" . $pgmess . "[0m\n";
-        } else {
-            $pgmess = pg_errormessage($dbid);
-            echo "[1;31;49m" . $pgmess . "[0m\n";
-            echo "[1;31;40m" . "ABORTED" . "[0m\n";
-            break;
+/* Play SQL commands */
+$point = uniqid(sprintf('%s/%s', $appClass, $class) , true);
+if (($err = $o->savePoint($point)) !== '') {
+    $action->exitError($err);
+}
+if ($updateExistingTable) {
+    print sprintf("Updating existing table '%s'...\n", $o->dbtable);
+    foreach ($sql as $k => $v) {
+        if (preg_match('/CREATE\s+  SEQUENCE\s+  (?:IF\s+NOT\s+EXISTS\s+)?  (?P<seqName>[a-z0-9_]+)/xi', $v, $m)) {
+            /* Do not recreate sequences (keep existing sequences untouched) */
+            print sprintf("[+] (%d/%d) Skipping sequence '%s'.\n", $m['seqName'], $k + 1, count($sql));
+            continue;
         }
+        print sprintf("[+] (%d/%d) Executing SQL:\n", $k + 1, count($sql));
+        print "\t--8<--\n";
+        print "\t" . str_replace("\n", "\n\t", $v) . "\n";
+        print "\t-->8--\n";
+        simpleQuery($o->dbaccess, $v, $res, false, false, true);
+        print "[+] Done.\n";
     }
+} else {
+    print sprintf("Creating table '%s'...\n", $o->dbtable);
+    /* Table does not exists: create it */
+    $o->Create();
+}
+if (($err = $o->commitPoint($point)) !== '') {
+    $action->exitError($err);
 }
 
-pg_close($dbid);
+print "\nDone.\n";
